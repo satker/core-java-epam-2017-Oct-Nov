5a64f444bd3cde763f9ca09a0f0552a8f5b905cd
elefus <elefus@yandex.ru>
2017-08-18 19:37:03 +0300

    Added materials se4 .

---
+++ src/main/java/com/epam/courses/jf/se4/Main.java
@@ -0,0 +1,16 @@
+package com.epam.courses.jf.se4;
+
+import java.io.IOError;
+import java.io.InputStream;
+import java.io.StringBufferInputStream;
+
+/**
+ * Created by Nikolai_Plokhoi on 8/16/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        InputStream a;
+        a.available()
+    }
+}
