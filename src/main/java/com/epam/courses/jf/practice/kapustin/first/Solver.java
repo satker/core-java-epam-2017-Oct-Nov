8effe9a93614bef86fce07fba068beedb51a0b93
elefus <elefus@yandex.ru>
2017-08-19 00:00:57 +0300

    Added stub for the first task.

---
+++ src/main/java/com/epam/courses/jf/practice/kapustin/first/Solver.java
@@ -0,0 +1,11 @@
+package com.epam.courses.jf.practice.kapustin.first;
+
+import com.epam.courses.jf.practice.common.first.ISolver;
+
+/**
+ * Реализация первого блока заданий.
+ */
+public class Solver implements ISolver {
+
+    // TODO выполнение задания
+}
