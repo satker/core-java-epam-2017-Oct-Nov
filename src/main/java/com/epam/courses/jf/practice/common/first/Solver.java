0ef091d515989ec25a35957b089d260a74dd6e37
Vadim Plaksin <plaksinvadimaleks@gmail.com>
2017-10-22 14:13:38 +0300

    String reader created

--- src/main/java/com/epam/courses/jf/practice/common/first/Solver.java
+++ src/main/java/com/epam/courses/jf/practice/common/first/Solver.java
@@ -1,5 +1,20 @@
 package com.epam.courses.jf.practice.common.first;
 
+import java.util.Scanner;
+
 public class Solver implements ISolver {
+
+    private static String[] readStrings(Scanner scanner) {
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = scanner.nextLine();
+        }
+
+        return result;
+
+    }
 
 }
