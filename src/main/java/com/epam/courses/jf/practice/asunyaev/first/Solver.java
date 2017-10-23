30876923a500cb62e284bfcd4bad71e8f2f0e31d
asunyaev <a.a.syunyaev@gmail.com>
2017-10-23 12:54:48 +0300

    Сделана заглушка Solver и реализован task1

--- src/main/java/com/epam/courses/jf/practice/asunyaev/first/Solver.java
+++ src/main/java/com/epam/courses/jf/practice/asunyaev/first/Solver.java
@@ -3,24 +3,27 @@ package com.epam.courses.jf.practice.asunyaev.first;
 import com.epam.courses.jf.practice.common.first.ISolver;
 
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Arrays;
 
 public class Solver implements ISolver {
 
     public static void main(String[] args) {
         Solver solver = new Solver();
-        solver.task1();
+        solver.task11();
     }
 
     @Override
     public void task1() {
         Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
         int currentLength = 0;
         String currentString;
         int maxLength = 0;
         int minLength = 80;
         String maxString = "";
         String minString = "";
-        int N = scan.nextInt();
+
         scan.nextLine();
 
         for (int i = 0; i < N; i++) {
@@ -41,5 +44,5 @@ public class Solver implements ISolver {
         System.out.printf("MIN (%d): \"%s\"%n", minLength, minString);
         System.out.printf("MAX (%d): \"%s\"%n", maxLength, maxString);
     }
-
+    
 }
