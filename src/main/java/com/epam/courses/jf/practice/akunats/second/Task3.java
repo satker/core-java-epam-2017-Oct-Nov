112f55bf0da60ef3bdb79b53c2d22b0b346d7a33
satker <satker73@gmail.com>
2017-10-23 16:29:43 +0300

    Third task in second part done!

---
+++ src/main/java/com/epam/courses/jf/practice/akunats/second/Task3.java
@@ -0,0 +1,23 @@
+package com.epam.courses.jf.practice.akunats.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask3;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Task3 implements ITestableTask3{
+    public List<String> sortPoems(Set<IPoem> poems, String author) {
+        List<String> result = new ArrayList<>();
+        for (IPoem poem : poems) {
+            if (poem.getAuthor().equals(author)) {
+                result = poem.getLines().stream()
+                        .sorted(Comparator.comparingInt(String::length))
+                        .collect(Collectors.toList());
+            }
+        }
+        return result;
+    }
+}
