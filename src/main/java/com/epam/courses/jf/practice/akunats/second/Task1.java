83d27b81b2aca86bde0f837661230f6cf5195454
satker <satker73@gmail.com>
2017-10-23 16:08:06 +0300

    First task in second part done!

---
+++ src/main/java/com/epam/courses/jf/practice/akunats/second/Task1.java
@@ -0,0 +1,30 @@
+package com.epam.courses.jf.practice.akunats.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask1;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Task1 implements ITestableTask1{
+    public List<String> reverseFile(File input, File output) {
+        List<String> result = new ArrayList<>();
+        try {
+            result = Files.lines(Paths.get(input.getPath()), Charset.forName("ISO-8859-1"))
+                    .collect(Collectors.toList());
+            Collections.reverse(result);
+            Files.write(Paths.get(output.getPath()), result, StandardCharsets.ISO_8859_1);
+        }
+        catch (IOException e){
+            System.out.println("Error");
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/epam/courses/jf/practice/akunats/second/Task2.java
@@ -0,0 +1,4 @@
+package com.epam.courses.jf.practice.akunats.second;
+
+public class Task2 {
+}
