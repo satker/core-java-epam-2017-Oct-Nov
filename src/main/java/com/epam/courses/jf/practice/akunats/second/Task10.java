05e503e2b8ad1b10f7f50eceb6143deb1063c3fe
satker <satker73@gmail.com>
2017-10-23 23:06:16 +0300

    Tenth task in second part done.

---
+++ src/main/java/com/epam/courses/jf/practice/akunats/second/Task10.java
@@ -0,0 +1,30 @@
+package com.epam.courses.jf.practice.akunats.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask10;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+public class Task10 implements ITestableTask10 {
+    public HashMap<String, Integer> countNumberWords(File input) {
+        HashMap<String, Integer> result = new HashMap<>();
+        try {
+            Files.lines(Paths.get(input.getPath()), Charset.forName("ISO-8859-1")).forEach(i -> {
+                for (String s : i.split(" ")) {
+                    if (result.containsKey(s)) {
+                        result.put(s, result.get(s) + 1);
+                    } else {
+                        result.put(s, 1);
+                    }
+                }
+            });
+        } catch (IOException e) {
+            System.out.println();
+        }
+        return result;
+    }
+}
