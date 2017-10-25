4859a6f82c6512208459379027d4eecba13a3823
hkryzhik <fraarson@gmail.com>
2017-10-26 00:06:41 +0300

    first 11 tasks solved, task 5 needs to be finished with regexps

---
+++ src/main/java/com/epam/courses/jf/practice/common/first/Solver.java
@@ -0,0 +1,387 @@
+package com.epam.courses.jf.practice.common.first;
+
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solver implements ISolver {
+
+    private static boolean isSorted(char[] characters){
+
+        for(int i = 1; i < characters.length; i++) {
+
+            if(characters[i - 1] > characters[i]) {
+
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void task1() {
+
+        Scanner inputData = new Scanner(System.in);
+
+        int size = 0;
+
+        if (inputData.hasNextInt()) {
+            size = Integer.parseInt(inputData.nextLine());
+        }
+
+        ArrayList<String> inputStringList = new ArrayList<>();
+
+        for(int i = 0; i < size; i++){
+            inputStringList.add(inputData.nextLine());
+        }
+
+        inputStringList.sort((s1, s2) -> s1.length() - s2.length());
+
+        System.out.println("MIN (" + inputStringList.get(0).length() + "): \"" +  inputStringList.get(0) + "\"");
+        System.out.println("MAX (" + inputStringList.get(inputStringList.size() - 1).length() +
+                "): \"" +  inputStringList.get(inputStringList.size() - 1) + "\"");
+    }
+
+    @Override
+    public void task2() {
+
+        Scanner inputData = new Scanner(System.in);
+
+        int size = 0;
+
+        if (inputData.hasNextInt()) {
+            size = Integer.parseInt(inputData.nextLine());
+        }
+
+        ArrayList<String> inputStringList = new ArrayList<>();
+
+        for(int i = 0; i < size; i++){
+            inputStringList.add(inputData.nextLine());
+        }
+
+        inputStringList.sort((String s1, String s2) -> {
+            if(s1.equals(s2)){
+                return 0;
+            }else {
+                if(s1.length() != s2.length()){
+                    return s1.length() - s2.length();
+                }else{
+                    for (int i = 0; i < s1.length(); i++) {
+                        if((int)s1.charAt(i) > (int)s2.charAt(i)){
+                            return -1;
+                        }else {
+                            return 1;
+                        }
+                    }
+                    return 0;
+                }
+            }
+        });
+
+        for (String string: inputStringList) {
+            System.out.printf("%d: \"%s\"\n", string.length(), string);
+        }
+
+    }
+
+    @Override
+    public void task3() {
+        Scanner inputData = new Scanner(System.in);
+
+        int size = 0;
+        int length = 0;
+
+
+        if (inputData.hasNextInt()) {
+            size = Integer.parseInt(inputData.nextLine());
+        }
+
+        ArrayList<String> inputStringList = new ArrayList<>();
+
+        for(int i = 0; i < size; i++){
+            inputStringList.add(inputData.nextLine());
+        }
+
+        for (String string : inputStringList) {
+            length += string.length();
+        }
+
+        final int stringLength = length / size;
+
+        System.out.println("AVERAGE(" + stringLength + ")");
+
+        inputStringList.forEach((String s) ->{
+
+            if(s.length() < stringLength){
+                System.out.printf("(%d): \"%s\"\n", s.length(), s);
+            }
+
+        });
+
+    }
+
+    @Override
+    public void task4() {
+        Scanner inputData = new Scanner(System.in);
+
+        int size = 0;
+
+        if (inputData.hasNextInt()) {
+            size = Integer.parseInt(inputData.nextLine());
+        }
+
+        String[] inputStrings = inputData.nextLine().split(" ");
+
+        ArrayList<HashSet> sets = new ArrayList<>();
+
+        for(String str : inputStrings){
+            HashSet<Character> characterSet = new HashSet<>();
+            for (Character ch : str.toLowerCase().toCharArray()) {
+                characterSet.add(ch);
+            }
+            sets.add(characterSet);
+        }
+
+        System.out.println(inputStrings[sets.indexOf(sets.stream().min((ch1, ch2) -> ch1.size() - ch2.size()).get())]);
+
+    }
+
+    //TODO: needs to be finished
+    @Override
+    public void task5() {
+        Scanner inputData = new Scanner(System.in);
+
+        int size = 0;
+
+        if (inputData.hasNextInt()) {
+            size = Integer.parseInt(inputData.nextLine());
+        }
+
+        String[] inputStrings = inputData.nextLine().split(" ");
+
+        for (String string : inputStrings) {
+
+            String lowerCaseString = string.toLowerCase();
+            Pattern pattern = Pattern.compile("([a-z])\\w+");
+            int count = 0;
+
+            if(lowerCaseString.matches("([a-z])\\w+")){
+
+                Matcher matcher = pattern.matcher(lowerCaseString);
+
+                while(matcher.find()){
+                    count++;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void task6() {
+        Scanner inputData = new Scanner(System.in);
+
+        int size = 0;
+
+        if (inputData.hasNextInt()) {
+            size = Integer.parseInt(inputData.nextLine());
+        }
+
+        String[] inputStrings = inputData.nextLine().split(" ");
+
+        ArrayList<String> strings = new ArrayList<>();
+        boolean flag = true;
+
+        for (String string : inputStrings) {
+
+            char[] charactersInWord = string.toCharArray();
+
+            if(isSorted(charactersInWord)){
+                String outWord = new String(charactersInWord);
+                System.out.println(outWord);
+            }else {
+                flag = false;
+            }
+        }
+        if(flag){
+            System.out.println("NOT FOUND");
+        }
+
+    }
+
+    @Override
+    public void task7() {
+        Scanner inputData = new Scanner(System.in);
+
+        int size = 0;
+
+        if (inputData.hasNextInt()) {
+            size = Integer.parseInt(inputData.nextLine());
+        }
+
+        String[] inputStrings = inputData.nextLine().split(" ");
+        boolean flag = true;
+
+        for (String word : inputStrings) {
+
+            char[] charactersInWord = word.toCharArray();
+            HashSet<Character> charactersSet = new HashSet<>();
+
+            for (char characterInWord : charactersInWord) {
+
+                charactersSet.add(characterInWord);
+            }
+
+            if(charactersInWord.length == charactersSet.size()){
+
+                String outString = new String(charactersInWord);
+
+                System.out.print(outString + " ");
+            }else {
+                flag = false;
+            }
+        }
+        if(flag){
+            System.out.println("NOT FOUND");
+        }
+    }
+
+    @Override
+    public void task8() {
+        Scanner inputData = new Scanner(System.in);
+
+        int size = 0;
+
+        if (inputData.hasNextInt()) {
+            try {
+                size = Integer.parseInt(inputData.nextLine());
+            }catch (NumberFormatException e){
+                System.out.println("Incorrect data format");
+            }
+        }
+
+        String[] inputStrings = inputData.nextLine().split(" ");
+
+        boolean flag = true;
+
+        for (String word : inputStrings) {
+
+            try {
+                if (Integer.parseInt(word) != 0) {
+                    char[] charactersInWord = word.toCharArray();
+                    int i1 = 0;
+                    int i2 = charactersInWord.length - 1;
+                    while (i2 > i1) {
+                        if (charactersInWord[i1] != charactersInWord[i2]) {
+                            flag = false;
+                            break;
+                        } else {
+                            flag = true;
+                        }
+                        ++i1;
+                        --i2;
+                    }
+                    if (flag) {
+                        System.out.println(word);
+                    }
+                }
+            }catch (NumberFormatException e){
+                continue;
+            }
+
+        }
+
+    }
+
+    @Override
+    public void task9() {
+        Scanner inputData = new Scanner(System.in);
+
+        int size = 0;
+
+        if (inputData.hasNextInt()) {
+            size = Integer.parseInt(inputData.nextLine());
+        }
+
+        int out = 1;
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++) {
+                System.out.print(out + " ");
+                out++;
+            }
+            System.out.println("\n");
+        }
+    }
+
+    @Override
+    public void task10() {
+        Scanner inputData = new Scanner(System.in);
+
+        String[] inputStrings = null;
+        int[] conversionArray = new int[3];
+
+        if(inputData.hasNext()){
+            inputStrings = inputData.nextLine().split(" ");
+        }
+
+        try {
+            for (int i = 0; i < 3; i++) {
+                conversionArray[i] = Integer.valueOf(inputStrings[i]);
+            }
+        }catch (NullPointerException e){
+            System.out.println("Wrong input type, try again nex time");
+        }
+
+        int a = conversionArray[0];
+        int b = conversionArray[1];
+        int c = conversionArray[2];
+
+
+        double d = Math.pow(b, 2) - 4*a*c;
+
+        double x1, x2;
+
+        if(d > 0){
+            x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4*a*c))/(2 * a);
+            x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4*a*c))/(2 * a);
+            System.out.printf("Two solutions: %f, %f\n", x1, x2);
+        }
+        if(d == 0){
+            x1 = -b / (2*a);
+            System.out.printf("One solution: %f\n", x1);
+        }
+        if(d < 0){
+            System.out.printf("No solution\n");
+        }
+
+
+
+
+    }
+
+    @Override
+    public void task11() {
+        Scanner inputData = new Scanner(System.in);
+
+        int input = 0;
+
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August",
+                "September", "October", "November", "December"};
+
+        if (inputData.hasNextInt()) {
+            input = Integer.parseInt(inputData.nextLine());
+        }
+
+        if(input > 0 && input <= 12){
+            System.out.println(months[input - 1]);
+        }else{
+            System.out.println("INCORRECT INPUT DATA");
+        }
+    }
+
+    @Override
+    public void task12() {
+
+    }
+}
---
+++ src/test/com/epam/courses/jf/practice/common/first/SolverTest.java
@@ -0,0 +1,157 @@
+package com.epam.courses.jf.practice.common.first;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.Assert.*;
+
+public class SolverTest {
+    @Test
+    public void task1() throws Exception {
+        String inputData = "4\nУнылая пора! Очей очарованье!\nПриятна мне твоя прощальная краса —\nЛюблю я пышное природы увяданье,\nВ багрец и в золото одетые леса,";
+        InputStream inputDataStream = new ByteArrayInputStream(inputData.getBytes("UTF-8"));
+        InputStream oldSystemStream = System.in;
+        try {
+            System.setIn(inputDataStream);
+            Solver solver = new Solver();
+            solver.task1();
+        } finally {
+            System.setIn(oldSystemStream);
+        }
+    }
+
+    @Test
+    public void task2() throws Exception {
+        String inputData = "5\nТихо струится река серебристая\nТкхо струится река серебристая\nВ царстве вечернем зеленой весны.\nСолнце садится" +
+                " за горы лесистые.\nРог золотой выплывает луны.\n";
+        InputStream inputDataStream = new ByteArrayInputStream(inputData.getBytes("UTF-8"));
+        InputStream oldSystemStream = System.in;
+        try {
+            System.setIn(inputDataStream);
+            Solver solver = new Solver();
+            solver.task2();
+        } finally {
+            System.setIn(oldSystemStream);
+        }
+    }
+
+    @Test
+    public void task3() throws Exception {
+        String inputData = "5\nПослушайте!\nВедь, если звезды зажигают -\nзначит - это кому-нибудь нужно?\nЗначит - " +
+                "кто-то хочет, чтобы они были?\nЗначит - кто-то называет эти плевочки жемчужиной?\n";
+        InputStream inputDataStream = new ByteArrayInputStream(inputData.getBytes("UTF-8"));
+        InputStream oldSystemStream = System.in;
+        try {
+            System.setIn(inputDataStream);
+            Solver solver = new Solver();
+            solver.task3();
+        } finally {
+            System.setIn(oldSystemStream);
+        }
+    }
+
+    @Test
+    public void task4() throws Exception {
+        String inputData = "4\nCake is a lie\n";
+        InputStream inputDataStream = new ByteArrayInputStream(inputData.getBytes("UTF-8"));
+        InputStream oldSystemStream = System.in;
+        try {
+            System.setIn(inputDataStream);
+            Solver solver = new Solver();
+            solver.task4();
+        } finally {
+            System.setIn(oldSystemStream);
+        }
+    }
+
+    @Test
+    public void task5() throws Exception{
+
+    }
+
+    @Test
+    public void task6() throws Exception{
+        String inputData = "12\nThe original and reference implementation Java compilers were originally released by Sun\n";
+        InputStream inputDataStream = new ByteArrayInputStream(inputData.getBytes("UTF-8"));
+        InputStream oldSystemStream = System.in;
+        try {
+            System.setIn(inputDataStream);
+            Solver solver = new Solver();
+            solver.task6();
+        } finally {
+            System.setIn(oldSystemStream);
+        }
+    }
+
+    @Test
+    public void task7() throws Exception{
+        String inputData = "11\nThe Java programming language is a general-purpose, concurrent, class-based, object-oriented language\n";
+        InputStream inputDataStream = new ByteArrayInputStream(inputData.getBytes("UTF-8"));
+        InputStream oldSystemStream = System.in;
+        try {
+            System.setIn(inputDataStream);
+            Solver solver = new Solver();
+            solver.task7();
+        } finally {
+            System.setIn(oldSystemStream);
+        }
+    }
+
+    @Test
+    public void task8() throws Exception{
+        String inputData = "15\nChapter 11 describes exceptions, chapter 13 describes binary compatibility... chapter 22 presents a syntactic grammar\n";
+        InputStream inputDataStream = new ByteArrayInputStream(inputData.getBytes("UTF-8"));
+        InputStream oldSystemStream = System.in;
+        try {
+            System.setIn(inputDataStream);
+            Solver solver = new Solver();
+            solver.task8();
+        } finally {
+            System.setIn(oldSystemStream);
+        }
+    }
+
+    @Test
+    public void task9() throws Exception{
+        String inputData = "5\n";
+        InputStream inputDataStream = new ByteArrayInputStream(inputData.getBytes("UTF-8"));
+        InputStream oldSystemStream = System.in;
+        try {
+            System.setIn(inputDataStream);
+            Solver solver = new Solver();
+            solver.task9();
+        } finally {
+            System.setIn(oldSystemStream);
+        }
+    }
+
+    @Test
+    public void task10() throws Exception{
+        String inputData = "2 7 0\n";
+        InputStream inputDataStream = new ByteArrayInputStream(inputData.getBytes("UTF-8"));
+        InputStream oldSystemStream = System.in;
+        try {
+            System.setIn(inputDataStream);
+            Solver solver = new Solver();
+            solver.task10();
+        } finally {
+            System.setIn(oldSystemStream);
+        }
+    }
+
+    @Test
+    public void task11() throws Exception{
+        String inputData = "1\n";
+        InputStream inputDataStream = new ByteArrayInputStream(inputData.getBytes("UTF-8"));
+        InputStream oldSystemStream = System.in;
+        try {
+            System.setIn(inputDataStream);
+            Solver solver = new Solver();
+            solver.task11();
+        } finally {
+            System.setIn(oldSystemStream);
+        }
+    }
+}
