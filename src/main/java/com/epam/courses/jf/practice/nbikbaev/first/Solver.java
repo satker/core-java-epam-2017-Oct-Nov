fdfa88a78b85f94ee430c2a85c96163ef5dd1a52
nbikbaev <niaz.bikbaev@gmail.com>
2017-10-28 20:25:35 +0300

    Task2 done

--- src/main/java/com/epam/courses/jf/practice/nbikbaev/first/Solver.java
+++ src/main/java/com/epam/courses/jf/practice/nbikbaev/first/Solver.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Solver implements ISolver {
