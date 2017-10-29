a1bd65c337f6b42c6bff0b04e7d89b793c9b6179
satker <satker73@gmail.com>
2017-10-29 22:05:10 +0300

    Update seven task.

--- src/main/java/com/epam/courses/jf/practice/akunats/second/Task7.java
+++ src/main/java/com/epam/courses/jf/practice/akunats/second/Task7.java
@@ -5,35 +5,33 @@ import com.epam.courses.jf.practice.common.second.ITestableTask7;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Task7 implements ITestableTask7{
+public class Task7 implements ITestableTask7 {
     public List<Integer> multiplyPolynomials(List<Integer> first, List<Integer> second) {
         List<Integer> result = new ArrayList<>();
         List<Integer> maxListSize = new ArrayList<>(first.size() >= second.size() ? first : second); // нходим коллекцию большей размерности
         // заполняем матрицу result возможным количеством нулей
-        int lengthMin = first.size() <= second.size() ? first.size() - 1 : second.size() - 1;
-        int lengthResult = 2 * lengthMin < maxListSize.size() ? maxListSize.size() - 1 : 2 * lengthMin;
-        for (int i = 0; i <= lengthResult; i++) {
+        int lengthMin = first.size() <= second.size() ? first.size() : second.size() ;
+        int lengthResult = (2 * lengthMin) < maxListSize.size() ? maxListSize.size() - 1 : (2 * lengthMin);
+        for (int i = 0; i < lengthResult; i++) {
             result.add(i, 0);
         }
+        List<Integer> minListSize = new ArrayList<>(first.size() < second.size() ? first : second);
         //////
-        for (int i = 0; i < maxListSize.size(); i++) {
-            // если индекс содержится в двух коллецкиях
-            // умножаем элементы (1) и записываем в индекс с сложенным показателем степени (2)
-            if (i <= lengthMin) {
-                if (i == 0) {
-                    result.set(0, first.get(i) * second.get(i));
-                } else {
-                    result.set(i + i, //(2)
-                            first.get(i) * second.get(i) // (1)
-                                    + (i + i < maxListSize.size()
-                                    && i + i >= (first.size() <= second.size() ? first.size() : second.size()) ?
-                                    maxListSize.get(i + i) : 0));
-                }
-                //если индекс не содержится в двух коллецкиях, оставляем тот элемент из большей коллекции
-            } else {
-                if (result.get(i) == 0) {
+        for (int i = 0; i < minListSize.size(); i++) {
+            for (int j = 0; j < maxListSize.size(); j++) {
+                // если индекс содержится в двух коллецкиях
+                // умножаем элементы (1) и записываем в индекс с сложенным показателем степени (2)
+                result.set(i + j, //(2)
+                        result.get(i + j)
+                                + maxListSize.get(j)
+                                * minListSize.get(i) // (1)
+                                /*+ (i + i < maxListSize.size()
+                                && i + i >= minListSize.size() ?
+                                maxListSize.get(i + j) : 0)*/);
+                /*if (result.get(i) == 0) {
                     result.set(i, maxListSize.get(i));
-                }
+                }*/
+                //если индекс не содержится в двух коллецкиях, оставляем тот элемент из большей коллекции
             }
         }
         return result;
