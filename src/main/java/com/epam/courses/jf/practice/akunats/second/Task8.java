7de5d8f601135de8eb1f94aeede1efbbbabeb13d
satker <satker73@gmail.com>
2017-10-30 13:55:26 +0300

    Update eighth task.

--- src/main/java/com/epam/courses/jf/practice/akunats/second/Task8.java
+++ src/main/java/com/epam/courses/jf/practice/akunats/second/Task8.java
@@ -5,30 +5,66 @@ import com.epam.courses.jf.practice.common.second.ITestableTask8;
 import java.util.Arrays;
 import java.util.Stack;
 
-public class Task8 implements ITestableTask8{
+public class Task8 implements ITestableTask8 {
+    //  1 не подходит, требуется следующая итерация
+    // 0 подходит
+    // -1 не подходит полностью
+    public int control(String a, String b) {
+        int result = 1;
+        for (int i = 0; i < a.length(); i++) {
+            char previousElement = a.toCharArray()[i];
+            char nowStr = b.toCharArray()[a.length() - 1 - i];
+            if ((nowStr == '(' && previousElement == ')') ||
+                    (nowStr == '[' && previousElement == ']') ||
+                    (nowStr == '{' && previousElement == '}')) {
+                result = 0;
+            } else if (previousElement == '(' || previousElement == '{'
+                    || previousElement == '[') {
+                result = -1;
+            }
+        }
+        return result;
+    }
+
     public boolean isNormalBrackets(String string) {
         Stack<String> stackSymbols = new Stack<>();
         // убираем все элементы кроме скобок и записываемв стек
         Arrays.stream(string.replaceAll("[^\\[\\]\\(\\)\\{\\}]+", "").split(""))
                 .forEach(stackSymbols::push);
         String previousElement = "";
+        String nowStr = "";
         boolean result = true;
         int i = 0;
         while (stackSymbols.size() > 0) {
             if (i == 0) {
-                previousElement = stackSymbols.pop();
+                String pop1 = stackSymbols.pop();
+                previousElement = previousElement.concat(pop1);
                 i++;
             } else if (i == 1) {
-                String nowStr = stackSymbols.pop();
-                if (!((nowStr.equals("(") && previousElement.equals(")")) ||
-                        (nowStr.equals("[") && previousElement.equals("]")) ||
-                        (nowStr.equals("{") && previousElement.equals("}")))) {
-                    result = false;
-                    break;
+                String pop2 = stackSymbols.pop();
+                nowStr = nowStr.concat(pop2);
+                if (pop2.equals("(") || pop2.equals("{") || pop2.equals("[")){
+                    if (control(previousElement, nowStr) == 0){
+                            previousElement = "";
+                            nowStr = "";
+                            i = 0;
+                    } else {
+                        nowStr = nowStr.replaceAll("[(\\]\\[)|(\\)\\()|(\\}\\{)]", "");
+                    }
                 } else {
-                    i = 0;
+                    if (control(previousElement, nowStr) == (-1)) {
+                        result = false;
+                        break;
+                    } else if (control(previousElement, nowStr) == 0) {
+                        previousElement = "";
+                        nowStr = "";
+                        i = 0;
+                    }
                 }
             }
+        }
+        if (nowStr.length() != 0 || previousElement.length() != 0){
+            result = false;
         }
         return result;
     }
--- src/test/java/com/epam/courses/jf/practice/akunats/second/Task17Test.java
+++ src/test/java/com/epam/courses/jf/practice/akunats/second/Task17Test.java
@@ -2,56 +2,85 @@ package com.epam.courses.jf.practice.akunats.second;
 
 import com.epam.courses.jf.practice.common.second.I2DPoint;
 import com.epam.courses.jf.practice.common.second.ITestableTask17;
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-import static junit.framework.TestCase.assertEquals;
 
 public class Task17Test {
-    @Test
-    public void analyze() {
-        Point p1 = new Point(1, 1);
-        Point p2 = new Point(3, 3);
+    public void test(Set<ITestableTask17.ISegment> segments, Set<I2DPoint> expected) throws Exception {
+        // Prepare
+        ITestableTask17 solver = new Task17();
 
-        Point p3 = new Point(2, 4);
-        Point p4 = new Point(2, 0);
+        // Run
+        Set<I2DPoint> result = solver.analyze(segments);
 
-        Point p5 = new Point(3, 1);
-        Point p6 = new Point(1, 4);
+        // Asserts
+        Set<I2DPoint> converted = new HashSet<>(result.size());
+        for (I2DPoint point : result) {
+            converted.add(new Point(point.getX(), point.getY()));
+        }
+        Assert.assertEquals(expected, converted);
+    }
 
-        Point p7 = new Point(3.5, 4);
-        Point p8 = new Point(5, 2);
+    /**
+     * Одна точка пересечения.
+     */
+    @org.junit.Test (timeout = 2000)
+    public void test1() throws Exception {
+        Set<ITestableTask17.ISegment> segments = new HashSet<>(2);
+        segments.add(new Segment(new Point(0, 0), new Point(2, 2)));
+        segments.add(new Segment(new Point(2, 0), new Point(0, 2)));
+        test(segments, Collections.<I2DPoint>singleton(new Point(1, 1)));
+    }
 
-        Point p9 = new Point(4, 1.5);
-        Point p10 = new Point(5, 0.5);
+    /**
+     * Две точки пересечения с разной абсциссой.
+     */
+    @org.junit.Test ()
+    public void test2() throws Exception {
+        Set<ITestableTask17.ISegment> segments = new HashSet<>(3);
+        segments.add(new Segment(new Point(0, 0), new Point(0, 2)));
+        segments.add(new Segment(new Point(2, 0), new Point(2, 2)));
+        segments.add(new Segment(new Point(0, 2), new Point(2, 2)));
+        test(segments, Collections.<I2DPoint>singleton(new Point(0, 2)));
+    }
 
-        Point p11 = new Point(5, 1.5);
-        Point p12 = new Point(4, 0.5);
-        ITestableTask17.ISegment s1 = new Segment(p1, p2);
-        ITestableTask17.ISegment s2 = new Segment(p3, p4);
-        ITestableTask17.ISegment s3 = new Segment(p5, p6);
-        ITestableTask17.ISegment s4 = new Segment(p7, p8);
-        ITestableTask17.ISegment s5 = new Segment(p9, p10);
-        ITestableTask17.ISegment s6 = new Segment(p11, p12);
-        Set<ITestableTask17.ISegment> set = new HashSet<>();
-        set.add(s1);
-        set.add(s4);
-        set.add(s2);
-        set.add(s5);
-        set.add(s3);
-        set.add(s6);
-        ArrayList<I2DPoint> result = new ArrayList<>(new Task17().analyze(set));
-        ArrayList<I2DPoint> control = new ArrayList<>();
-        control.add(new Point(4.5, 1.0));
-        control.add(new Point(2.0, 2.5));
-        control.add(new Point(2.0, 2.0));
-        assertEquals(control.size(), result.size());
-        for (int i = 0; i < control.size(); i++) {
-            assertEquals(control.get(i).getX(), result.get(i).getX());
-            assertEquals(control.get(i).getY(), result.get(i).getY());
+    /**
+     * Три точки пересечения, две из них имеют минимальную абсциссу
+     */
+    @org.junit.Test (timeout = 2000)
+    public void test3() throws Exception {
+        Set<ITestableTask17.ISegment> segments = new HashSet<>(3);
+        segments.add(new Segment(new Point(0, 0), new Point(0, 3)));
+        segments.add(new Segment(new Point(0, 3), new Point(3, 0)));
+        segments.add(new Segment(new Point(0, 0), new Point(3, 3)));
+        test(segments, new HashSet<I2DPoint>(Arrays.asList(new Point(0, 3), new Point(0, 0))));
+    }
+
+    /**
+     * Отрезок на двумерной плоскости.
+     */
+    private class Segment implements ITestableTask17.ISegment {
+
+        private final I2DPoint FIRST;
+
+        private final I2DPoint SECOND;
+
+        private Segment(I2DPoint start, I2DPoint end) {
+            FIRST = start;
+            SECOND = end;
+        }
+
+        @Override
+        public I2DPoint first() {
+            return FIRST;
+        }
+
+        @Override
+        public I2DPoint second() {
+            return SECOND;
         }
     }
 
