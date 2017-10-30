ceae691d056fb06fbf0a0761146bd500d70a7e9d
klm-1 <klimenko.v.2.0@gmail.com>
2017-10-30 20:02:33 +0300

    Added input check to task11

--- src/main/java/com/epam/courses/jf/practice/klimenko/first/SolverImpl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/first/SolverImpl.java
@@ -228,49 +228,53 @@ public class SolverImpl implements ISolver {
 
     @Override
     public void task11() {
-        int monthIndex = new Scanner(System.in).nextInt();
-        String monthName = "???";
+        String monthName = "INCORRECT INPUT DATA";
 
-        switch (monthIndex) {
-            case 1:
-                monthName = "January";
-                break;
-            case 2:
-                monthName = "February";
-                break;
-            case 3:
-                monthName = "March";
-                break;
-            case 4:
-                monthName = "April";
-                break;
-            case 5:
-                monthName = "May";
-                break;
-            case 6:
-                monthName = "June";
-                break;
-            case 7:
-                monthName = "July";
-                break;
-            case 8:
-                monthName = "August";
-                break;
-            case 9:
-                monthName = "September";
-                break;
-            case 10:
-                monthName = "October";
-                break;
-            case 11:
-                monthName = "November";
-                break;
-            case 12:
-                monthName = "December";
-                break;
-            default:
-                monthName = "INCORRECT INPUT DATA";
-                break;
+        try {
+            int monthIndex = new Scanner(System.in).nextInt();
+
+            switch (monthIndex) {
+                case 1:
+                    monthName = "January";
+                    break;
+                case 2:
+                    monthName = "February";
+                    break;
+                case 3:
+                    monthName = "March";
+                    break;
+                case 4:
+                    monthName = "April";
+                    break;
+                case 5:
+                    monthName = "May";
+                    break;
+                case 6:
+                    monthName = "June";
+                    break;
+                case 7:
+                    monthName = "July";
+                    break;
+                case 8:
+                    monthName = "August";
+                    break;
+                case 9:
+                    monthName = "September";
+                    break;
+                case 10:
+                    monthName = "October";
+                    break;
+                case 11:
+                    monthName = "November";
+                    break;
+                case 12:
+                    monthName = "December";
+                    break;
+                default:
+                    break;
+            }
+        } catch (InputMismatchException e) {
+            // Ignore exception
         }
 
         System.out.println(monthName);
