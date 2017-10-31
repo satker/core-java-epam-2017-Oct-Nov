e49b9f833a7886cc609372e29cb82f6dc69e1fea
satker <satker73@gmail.com>
2017-10-31 19:54:58 +0300

    Little update.

--- src/main/java/com/epam/courses/jf/practice/akunats/second/Task11.java
+++ src/main/java/com/epam/courses/jf/practice/akunats/second/Task11.java
@@ -3,29 +3,11 @@ package com.epam.courses.jf.practice.akunats.second;
 import com.epam.courses.jf.practice.common.second.ITestableTask11;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Task11 implements ITestableTask11 {
     public String emulate(ArrayList<String> peoples) {
-//        int i = 0;
-//        while (peoples.size() > 1) {
-//            ArrayList<String> interList = new ArrayList<>();
-//            for (int j = 0; j < peoples.size(); j++) {
-//                if (interList.size() >= peoples.size() - 1) {
-//                    break;
-//                }
-//                if (j == i) {
-//                    interList.add(peoples.get(i));
-//                    if (i + 2 < peoples.size()) {
-//                        i += 2;
-//                    } else {
-//                        i = ((i + 2) % (peoples.size() - 1)) == 1 ? 0 : 1;
-//                    }
-//                }
-//            }
-//            peoples.removeAll(interList);
-//        }
-//        return peoples.get(0);
         if (peoples == null || peoples.isEmpty()) {
             throw new IllegalArgumentException("Linked list not provided!");
         }
@@ -46,31 +28,18 @@ public class Task11 implements ITestableTask11 {
     }
 
     public String emulate(LinkedList<String> peoples) {
-//        int i = 0;
-//        while (peoples.size() > 1) {
-//            Iterator<String> iter = peoples.iterator();
-//            while (iter.hasNext()) {
-//                String next = iter.next();
-//                if (peoples.size() == 1) {
-//                    break;
-//                }
-//                if (peoples.indexOf(next) == i) {
-//                    iter.remove();
-//                    if (i + 1 < peoples.size()) {
-//                        i += 1;
-//                    } else {
-//                        if (peoples.size() == 1) {
-//                            break;
-//                        }
-//                        i = i % (peoples.size() - 1);
-//                    }
-//                }
-//            }
-//        }
-//        return peoples.get(0);
-        if (peoples == null || peoples.isEmpty()) {
-            throw new IllegalArgumentException("Array list not provided!");
+        int i = 0;
+        while (peoples.size() > 1) {
+            Iterator<String> iterator = peoples.iterator();
+                while (iterator.hasNext()) {
+                    iterator.next();
+                    if (i == 0) iterator.remove();
+                    if (iterator.hasNext()) {
+                        iterator.next();
+                        if (i == 1) iterator.remove();
+                    } else i = (i == 0 ? 1 : 0);
+                }
         }
-        return emulate(new ArrayList<>(peoples));
+        return peoples.get(0);
     }
 }
