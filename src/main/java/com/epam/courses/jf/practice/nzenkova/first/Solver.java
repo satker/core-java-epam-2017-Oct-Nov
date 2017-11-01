484f505f0782cc39ba53c17921406f8106870021
nzenkova <natalka22-96@hotmail.com>
2017-11-01 19:12:15 +0300

    First

---
+++ src/main/java/com/epam/courses/jf/practice/nzenkova/first/Solver.java
@@ -0,0 +1,9 @@
+package com.epam.courses.jf.practice.nzenkova.first;
+
+import com.epam.courses.jf.practice.common.first.ISolver;
+
+/**
+ * Created by natal on 01.11.2017.
+ */
+public class Solver implements ISolver {
+}
