aa43fe5d439e83ef6b12ca569f6d2ca4d2ace162
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-02 19:08:29 +0300

    Update solution of 2.4

--- src/main/java/com/epam/courses/jf/practice/sskovalevskiy/second/ITestableTask4Impl.java
+++ src/main/java/com/epam/courses/jf/practice/sskovalevskiy/second/ITestableTask4Impl.java
@@ -2,6 +2,9 @@ package com.epam.courses.jf.practice.sskovalevskiy.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask4;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -10,13 +13,15 @@ import java.util.Set;
 public class ITestableTask4Impl implements ITestableTask4 {
     @Override
     public Set<Integer> intersection(Set<Integer> first, Set<Integer> second) {
-        first.retainAll(second);
-        return first;
+        Set<Integer> result = new HashSet<>(first);
+        result.retainAll(second);
+        return result;
     }
 
     @Override
     public Set<Integer> union(Set<Integer> first, Set<Integer> second) {
-        first.addAll(second);
-        return first;
+        Set<Integer> result = new HashSet<>(first);
+        result.addAll(second);
+        return result;
     }
 }
