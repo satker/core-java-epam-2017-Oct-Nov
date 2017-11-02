a9a186d9da14ae9a9aa533be87631890117f1793
nzenkova <natalka22-96@hotmail.com>
2017-11-02 17:23:47 +0300

    1-19 Задачи, исправление 14(3)

--- src/main/java/com/epam/courses/jf/practice/nzenkova/first/Solver.java
+++ src/main/java/com/epam/courses/jf/practice/nzenkova/first/Solver.java
@@ -305,7 +305,7 @@ public class Solver implements ISolver {
     public void task14(){
         Scanner scanner = new Scanner(System.in);
         int number = Integer.parseInt(scanner.nextLine());
-        int numberMax = 0, counter = 1;
+        int numberMax = 0, counter = 0;
         int number1 = 0, number2 = 0;
         number1 = scanner.nextInt();
         for(int i = 0; i < number - 1; ++i){
@@ -314,8 +314,8 @@ public class Solver implements ISolver {
                 ++counter; number1 = number2;
             }
             else{
-                if(numberMax < counter) numberMax = counter;
-                counter = 1;
+                numberMax = counter;
+                counter = 0;
                 number1 = number2;
             }
         }
