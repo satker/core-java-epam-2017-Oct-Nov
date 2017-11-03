4b3b491a2f5abc2ca648d8e037f6d85b41a7f807
vakhonin <isvakhonin@yandex.ru>
2017-11-03 21:18:03 +0300

    1

---
+++ src/main/java/com/epam/courses/jf/practice/vakhonin/Solver.java
@@ -0,0 +1,9 @@
+package com.epam.courses.jf.practice.vakhonin;
+
+import com.epam.courses.jf.practice.common.first.ISolver;
+
+/**
+ * Created by igorvahonin on 03.11.17.
+ */
+public class Solver implements ISolver {
+}
