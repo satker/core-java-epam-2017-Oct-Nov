50a84495025fa4c7c8edd903de60a648416b31a3
Vadim Plaksin <plaksinvadimaleks@gmail.com>
2017-11-04 21:44:32 +0300

    task 2.5 done

--- src/main/java/com/epam/courses/jf/practice/vplaksin/second/Task5.java
+++ src/main/java/com/epam/courses/jf/practice/vplaksin/second/Task5.java
@@ -2,13 +2,26 @@ package com.epam.courses.jf.practice.vplaksin.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask5;
 
+import java.math.BigDecimal;
 import java.util.List;
 
-public class Task5 implements ITestableTask5{
+public class Task5 implements ITestableTask5 {
 
     @Override
     public double calcResistance(List<IMeasurement> measurements) {
-        return 0;
+
+        BigDecimal sumUI = BigDecimal.ZERO;
+        BigDecimal sumII = BigDecimal.ZERO;
+
+        for (IMeasurement measurement : measurements) {
+            BigDecimal u = BigDecimal.valueOf(measurement.getVoltage());
+            BigDecimal i = BigDecimal.valueOf(measurement.getCurrent());
+            sumUI.add(u.multiply(i));
+            sumII.add(i.multiply(i));
+        }
+
+        return sumUI.divide(sumII).doubleValue();
+
     }
 
 }
