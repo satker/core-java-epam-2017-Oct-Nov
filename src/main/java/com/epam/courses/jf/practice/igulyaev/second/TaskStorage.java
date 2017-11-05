83a2e0b8ad1310027a5a561788a73505f1774d3f
gulyaev13 <gulyaev13@gmail.com>
2017-11-06 00:23:25 +0300

    Add tasks 2.1, 2.3, 2.4, 2.6 to TaskStorage

--- src/main/java/com/epam/courses/jf/practice/igulyaev/second/TaskStorage.java
+++ src/main/java/com/epam/courses/jf/practice/igulyaev/second/TaskStorage.java
@@ -1,7 +1,7 @@
 package com.epam.courses.jf.practice.igulyaev.second;
 
 import com.epam.courses.jf.practice.common.second.ITaskStorage;
-import com.epam.courses.jf.practice.common.second.ITestableTask2;
+import com.epam.courses.jf.practice.common.second.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,7 +11,11 @@ public class TaskStorage implements ITaskStorage {
 
     public TaskStorage() {
         taskMap = new HashMap<>();
+        taskMap.put(ITestableTask1.class.getSimpleName(), new TestableTask1());
         taskMap.put(ITestableTask2.class.getSimpleName(), new TestableTask2());
+        taskMap.put(ITestableTask3.class.getSimpleName(), new TestableTask3());
+        taskMap.put(ITestableTask4.class.getSimpleName(), new TestableTask4());
+        taskMap.put(ITestableTask6.class.getSimpleName(), new TestableTask6());
     }
 
     @Override
