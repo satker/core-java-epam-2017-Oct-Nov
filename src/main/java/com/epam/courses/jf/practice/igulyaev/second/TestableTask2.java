94c69ecc9ae4c33db45b856f090e36929c8bdf3a
gulyaev13 <gulyaev13@gmail.com>
2017-11-05 22:11:37 +0300

    Task 2.2 implemented

---
+++ src/main/java/com/epam/courses/jf/practice/igulyaev/second/TestableTask2.java
@@ -0,0 +1,23 @@
+package com.epam.courses.jf.practice.igulyaev.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask2;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class TestableTask2 implements ITestableTask2 {
+    @Override
+    public Set<File> getFiles(File directory) {
+        Set<File> files = new HashSet<>();
+        try {
+            Files.walk(directory.toPath(), Integer.MAX_VALUE).forEach(p -> files.add(p.toFile()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return files;
+    }
+}
