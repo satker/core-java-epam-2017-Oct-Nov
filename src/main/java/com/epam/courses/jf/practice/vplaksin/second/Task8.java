eb3994b4e34c0b5dcaf6dd3732f9c2349a2d0cf9
Vadim Plaksin <plaksinvadimaleks@gmail.com>
2017-11-05 14:29:45 +0300

    task 2.8 done

--- src/main/java/com/epam/courses/jf/practice/vplaksin/second/Task8.java
+++ src/main/java/com/epam/courses/jf/practice/vplaksin/second/Task8.java
@@ -2,11 +2,34 @@ package com.epam.courses.jf.practice.vplaksin.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask8;
 
-public class Task8 implements ITestableTask8{
+import java.util.Stack;
+
+public class Task8 implements ITestableTask8 {
 
     @Override
     public boolean isNormalBrackets(String string) {
-        return false;
+
+        char[] chars = string.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char c : chars) {
+            if (c == '{' || c == '(' || c == '[') {
+                stack.push(c);
+            } else if (c == '}' || c == ')' || c == ']') {
+                if (stack.empty()) {
+                    return false;
+                } else {
+                    char checkChar = stack.pop();
+                    if (!(checkChar == '{' && c == '}'
+                            || checkChar == '(' && c == ')'
+                            || checkChar == '[' && c == ']')) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return stack.empty();
+
     }
 
 }
