4cdca1bc5819ad5519f9e9bc8211bc2f68378877
gulyaev13 <gulyaev13@gmail.com>
2017-11-05 23:22:56 +0300

    Move to switch in TaskStorage

--- src/main/java/com/epam/courses/jf/practice/igulyaev/second/TaskStorage.java
+++ src/main/java/com/epam/courses/jf/practice/igulyaev/second/TaskStorage.java
@@ -1,22 +1,15 @@
 package com.epam.courses.jf.practice.igulyaev.second;
 
 import com.epam.courses.jf.practice.common.second.ITaskStorage;
-import com.epam.courses.jf.practice.common.second.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class TaskStorage implements ITaskStorage {
-    private static Map<String, Object> taskMap;
-
-    static {
-        taskMap = new HashMap<>();
-        taskMap.put(ITestableTask2.class.getSimpleName(), new TestableTask2());
-    }
-
-
     @Override
     public <T extends ITestableTask> T getSolver(Class<T> taskInterface) {
-        return (T) taskMap.get(taskInterface.getSimpleName());
+        switch (taskInterface.getSimpleName()){
+            case "ITestableTask2":
+                return (T) new TestableTask2();
+            default:
+                return null;
+        }
     }
 }
