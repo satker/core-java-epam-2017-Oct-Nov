e76025c798bb90c6fb8d635d624e531362a000ba
gulyaev13 <gulyaev13@gmail.com>
2017-11-06 00:11:37 +0300

    TaskStorage back to Map

--- src/main/java/com/epam/courses/jf/practice/igulyaev/second/TaskStorage.java
+++ src/main/java/com/epam/courses/jf/practice/igulyaev/second/TaskStorage.java
@@ -1,15 +1,21 @@
 package com.epam.courses.jf.practice.igulyaev.second;
 
 import com.epam.courses.jf.practice.common.second.ITaskStorage;
+import com.epam.courses.jf.practice.common.second.ITestableTask2;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TaskStorage implements ITaskStorage {
+    private Map<String, ITestableTask> taskMap;
+
+    public TaskStorage() {
+        taskMap = new HashMap<>();
+        taskMap.put(ITestableTask2.class.getSimpleName(), new TestableTask2());
+    }
+
     @Override
     public <T extends ITestableTask> T getSolver(Class<T> taskInterface) {
-        switch (taskInterface.getSimpleName()){
-            case "ITestableTask2":
-                return (T) new TestableTask2();
-            default:
-                return null;
-        }
+        return (T) taskMap.get(taskInterface);
     }
 }
