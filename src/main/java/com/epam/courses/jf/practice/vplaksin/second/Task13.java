bb2cfaa31a58297f566e8db059237f476510faf2
Vadim Plaksin <plaksinvadimaleks@gmail.com>
2017-11-05 17:30:29 +0300

    task 2.13 done

--- src/main/java/com/epam/courses/jf/practice/vplaksin/second/Task13.java
+++ src/main/java/com/epam/courses/jf/practice/vplaksin/second/Task13.java
@@ -2,11 +2,49 @@ package com.epam.courses.jf.practice.vplaksin.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask13;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Task13 implements ITestableTask13 {
 
     @Override
     public AbstractGraph createGraph(int numberNodes) {
-        return null;
+
+        return new Graph(numberNodes);
+
+    }
+
+    private class Graph extends AbstractGraph {
+
+        private HashMap<Integer, HashSet<Integer>> edges = new HashMap<>();
+
+        public Graph(int numberNodes) {
+            super(numberNodes);
+            for (int i = 0; i < numberNodes; i++) {
+                edges.put(i, new HashSet<>());
+            }
+        }
+
+        @Override
+        public void addEdge(int first, int second) {
+            if (edges.containsKey(first) && !edges.get(first).contains(second)) {
+                edges.get(first).add(second);
+                edges.get(second).add(first);
+            }
+        }
+
+        @Override
+        public void removeEdge(int first, int second) {
+            if (edges.containsKey(first) && edges.get(first).contains(second)) {
+                edges.get(first).remove(second);
+                edges.get(second).remove(first);
+            }
+        }
+
+        @Override
+        public boolean isExistEdge(int first, int second) {
+            return edges.get(first).contains(second);
+        }
     }
 
 }
