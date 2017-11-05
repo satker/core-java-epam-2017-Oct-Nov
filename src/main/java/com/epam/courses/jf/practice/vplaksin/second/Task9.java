e1ae38d77d33b0482f883bbc45f4d0d40bc453fa
Vadim Plaksin <plaksinvadimaleks@gmail.com>
2017-11-05 14:43:57 +0300

    task 2.9 done

--- src/main/java/com/epam/courses/jf/practice/vplaksin/second/Task9.java
+++ src/main/java/com/epam/courses/jf/practice/vplaksin/second/Task9.java
@@ -3,13 +3,30 @@ package com.epam.courses.jf.practice.vplaksin.second;
 import com.epam.courses.jf.practice.common.second.ITestableTask9;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class Task9 implements ITestableTask9 {
 
     @Override
     public HashSet<String> getUniqueWords(File input) {
-        return null;
+
+        HashSet<String> result = new HashSet<>();
+
+        try (Scanner scanner = new Scanner(new FileReader(input))) {
+
+            while (scanner.hasNext()){
+                result.add(scanner.next().toLowerCase());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+
     }
 
 }
