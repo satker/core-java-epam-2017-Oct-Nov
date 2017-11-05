bde7e88e9a2ed5414a8823f39a419a8e0c728755
gulyaev13 <gulyaev13@gmail.com>
2017-11-06 00:13:22 +0300

    Task 2.1 implemented

---
+++ src/main/java/com/epam/courses/jf/practice/igulyaev/second/TestableTask1.java
@@ -0,0 +1,31 @@
+package com.epam.courses.jf.practice.igulyaev.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask1;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TestableTask1 implements ITestableTask1 {
+    @Override
+    public List<String> reverseFile(File input, File output) {
+        List<String> stringList = new LinkedList<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(input))){
+            for(String str = bufferedReader.readLine(); str != null;){
+                stringList.add(str);
+            }
+        }catch(IOException e){
+            e.printStackTrace();//TODO: Add logger
+        }
+        Collections.reverse(stringList);
+        try(BufferedWriter bufferedReader = new BufferedWriter(new FileWriter(output))){
+            for(String str : stringList){
+                bufferedReader.write(str);
+            }
+        }catch(IOException e){
+            e.printStackTrace();//TODO: Add logger
+        }
+        return stringList;
+    }
+}
