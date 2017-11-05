e31fd6f5e8fe58b9514278668036f23cd6b79d5e
Vadim Plaksin <plaksinvadimaleks@gmail.com>
2017-11-05 13:59:20 +0300

    task 2.7 done

--- src/main/java/com/epam/courses/jf/practice/vplaksin/second/Task7.java
+++ src/main/java/com/epam/courses/jf/practice/vplaksin/second/Task7.java
@@ -2,11 +2,34 @@ package com.epam.courses.jf.practice.vplaksin.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask7;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Task7 implements ITestableTask7{
+public class Task7 implements ITestableTask7 {
+
     @Override
     public List<Integer> multiplyPolynomials(List<Integer> first, List<Integer> second) {
-        return null;
+
+        List<Integer> result = new ArrayList<>(first.size() + second.size() - 1);
+
+        if (first.size() == 1 && first.get(0) == 0
+                || second.size() == 1 && second.get(0) == 0) {
+            result.add(0);
+            return result;
+        }
+
+        for (int i = 0; i < first.size() + second.size() - 1; i++) {
+            result.add(0);
+        }
+
+        for (int i = 0; i < first.size(); i++) {
+            for (int j = 0; j < second.size(); j++) {
+                result.set(i + j, result.get(i + j) + first.get(i) * second.get(j));
+            }
+        }
+
+        return result;
+
     }
+
 }
