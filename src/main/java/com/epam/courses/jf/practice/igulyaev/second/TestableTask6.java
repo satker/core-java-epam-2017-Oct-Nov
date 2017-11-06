825d30fb3214cba8d3ee20a6b6b8e736be8b54e1
gulyaev13 <gulyaev13@gmail.com>
2017-11-06 13:56:27 +0300

    Task 2.6 max key implemented on stream

--- src/main/java/com/epam/courses/jf/practice/igulyaev/second/TestableTask6.java
+++ src/main/java/com/epam/courses/jf/practice/igulyaev/second/TestableTask6.java
@@ -9,7 +9,8 @@ import java.util.Map;
 public class TestableTask6 implements ITestableTask6 {
     @Override
     public HashMap<Integer, Integer> addPolynomials(HashMap<Integer, Integer> first, HashMap<Integer, Integer> second) {
-        int n  = Math.max(Collections.max(first.keySet()), Collections.max(second.keySet()));
+        int n  = Math.max(first.keySet().stream().max(Integer::compareTo).orElse(0),
+                second.keySet().stream().max(Integer::compareTo).orElse(0));
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < n; ++i){
             Integer firstInt = first.get(i);
@@ -25,5 +26,10 @@ public class TestableTask6 implements ITestableTask6 {
             }
         }
         return (HashMap<Integer,Integer>)map;
+    }
+
+    public static void main(String[] args) {
+        TestableTask6 task6 = new TestableTask6();
+        System.out.println(task6.addPolynomials(new HashMap<>(), new HashMap<>()));
     }
 }
