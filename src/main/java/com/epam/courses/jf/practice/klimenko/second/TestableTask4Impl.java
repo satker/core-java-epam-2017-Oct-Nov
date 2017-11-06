73d5583660f639895c8f3e6699a4528ecaa00eb8
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-06 23:46:11 +0300

    Implemented task2-4

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask4Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask4Impl.java
@@ -2,16 +2,21 @@ package com.epam.courses.jf.practice.klimenko.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask4;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TestableTask4Impl implements ITestableTask4 {
     @Override
     public Set<Integer> intersection(Set<Integer> first, Set<Integer> second) {
-        return null;
+        return first.stream().filter(i -> second.contains(i)).collect(Collectors.toSet());
     }
 
     @Override
     public Set<Integer> union(Set<Integer> first, Set<Integer> second) {
-        return null;
+        Set<Integer> ret = new HashSet<>();
+        ret.addAll(first);
+        ret.addAll(second);
+        return ret;
     }
 }
