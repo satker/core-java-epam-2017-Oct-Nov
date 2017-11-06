ca8534439db8259e530bf684e51c4ada864657e0
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-07 00:37:31 +0300

    Implemented task2-7

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask7Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask7Impl.java
@@ -2,11 +2,18 @@ package com.epam.courses.jf.practice.klimenko.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask7;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class TestableTask7Impl implements ITestableTask7 {
     @Override
     public List<Integer> multiplyPolynomials(List<Integer> first, List<Integer> second) {
-        return null;
+        Integer[] ret = new Integer[first.size() + second.size()];
+        for(int firstPower = 0; firstPower < first.size(); ++firstPower) {
+            for(int secondPower = 0; secondPower < second.size(); ++secondPower) {
+                ret[firstPower + secondPower] = first.get(firstPower) * second.get(secondPower);
+            }
+        }
+        return Arrays.asList(ret);
     }
 }
