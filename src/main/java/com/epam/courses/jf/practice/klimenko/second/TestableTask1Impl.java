7f2757edfed3005f48d6ff82c0d9e5ff69ccd0d3
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-06 23:14:54 +0300

    Fixed task2-1

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask1Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask1Impl.java
@@ -6,38 +6,22 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TestableTask1Impl implements ITestableTask1 {
     @Override
     public List<String> reverseFile(File input, File output) {
-        List<String> list = new ArrayList<>();
+        List<String> list;
 
-        try (FileReader reader = new FileReader(input)) {
-            StringBuilder line = new StringBuilder();
-            for(;;) {
-                int ch = reader.read();
-                if( ch == '\n' || ch == -1 ) {
-                    list.add(line.toString());
-                    line.setLength(0);
-                    if (ch == -1) {
-                        break;
-                    }
-                } else {
-                    line.append(Character.toChars(ch));
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-
-        try (FileWriter writer = new FileWriter(output)) {
-            for(int i = list.size() - 1; i >= 0; --i) {
-                writer.write(list.get(i));
-                writer.write('\n');
-            }
+        try {
+            list = Files.lines(input.toPath()).collect(Collectors.toList());
+            Collections.reverse(list);
+            Files.write(output.toPath(), list);
+            Collections.reverse(list);
         } catch (IOException e) {
             e.printStackTrace();
             return null;
