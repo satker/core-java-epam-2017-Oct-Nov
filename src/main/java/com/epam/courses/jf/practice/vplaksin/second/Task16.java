3a7dd080c3eceef788c080091b069e738374cd66
Vadim Plaksin <plaksinvadimaleks@gmail.com>
2017-11-06 14:34:04 +0300

    task 2.16 modified

--- src/main/java/com/epam/courses/jf/practice/vplaksin/second/Task16.java
+++ src/main/java/com/epam/courses/jf/practice/vplaksin/second/Task16.java
@@ -85,6 +85,7 @@ public class Task16 implements ITestableTask16 {
                 String s = reader.readLine();
                 String[] splited = s.split("\\s");
                 final I2DPoint center = new Point2D(Double.parseDouble(splited[0]), Double.parseDouble(splited[1]));
+                result = new TreeMap<>(Comparator.comparingDouble(point -> distance(center, point)));
 
                 while ((s = reader.readLine()) != null) {
                     splited = s.split("\\s");
