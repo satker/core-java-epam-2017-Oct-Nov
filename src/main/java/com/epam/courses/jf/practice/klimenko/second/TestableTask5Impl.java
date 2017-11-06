1a2b1528ccfa5f775453c13af9e8e4340dfe49ac
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-06 23:57:13 +0300

    Implemented task2-5

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask5Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask5Impl.java
@@ -2,11 +2,21 @@ package com.epam.courses.jf.practice.klimenko.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask5;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 public class TestableTask5Impl implements ITestableTask5 {
     @Override
     public double calcResistance(List<IMeasurement> measurements) {
-        return 0;
+        BigDecimal sumPower = new BigDecimal(0);
+        BigDecimal sumCurrentSquared = new BigDecimal(0);
+
+        for(IMeasurement m : measurements) {
+            sumPower = sumPower.add( new BigDecimal( m.getCurrent() * m.getVoltage() ) );
+            sumCurrentSquared = sumCurrentSquared.add( new BigDecimal( m.getCurrent() * m.getCurrent() ) );
+        }
+
+        return sumPower.divide(sumCurrentSquared, 12, RoundingMode.HALF_UP).doubleValue();
     }
 }
