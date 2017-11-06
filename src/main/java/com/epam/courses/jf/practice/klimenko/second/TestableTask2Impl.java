e898e82a0f0ac4775ddac8d8768d98ef5f9fd3bc
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-06 23:28:02 +0300

    Implemented task2-2

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask2Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask2Impl.java
@@ -3,11 +3,26 @@ package com.epam.courses.jf.practice.klimenko.second;
 import com.epam.courses.jf.practice.common.second.ITestableTask2;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 public class TestableTask2Impl implements ITestableTask2 {
     @Override
     public Set<File> getFiles(File directory) {
-        return null;
+        Set<File> ret = new HashSet<>();
+
+        try {
+            for (File file : directory.listFiles()) {
+                ret.add(file);
+                if (file.isDirectory()) {
+                    ret.addAll(getFiles(file));
+                }
+            }
+        } catch (NullPointerException e) {
+            return null;
+        }
+
+        return ret;
     }
 }
