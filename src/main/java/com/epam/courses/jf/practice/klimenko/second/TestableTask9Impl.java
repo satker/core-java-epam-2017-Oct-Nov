91fb3023bbf29df66d51364bc238c8de40385857
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-07 00:53:06 +0300

    Implemented task2-9

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask9Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask9Impl.java
@@ -3,11 +3,24 @@ package com.epam.courses.jf.practice.klimenko.second;
 import com.epam.courses.jf.practice.common.second.ITestableTask9;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class TestableTask9Impl implements ITestableTask9 {
     @Override
     public HashSet<String> getUniqueWords(File input) {
-        return null;
+        try (FileReader reader = new FileReader(input)) {
+            HashSet<String> ret = new HashSet<>();
+            Scanner scanner = new Scanner(reader);
+            while (scanner.hasNext()) {
+                ret.add(scanner.next().toLowerCase());
+            }
+            return ret;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
