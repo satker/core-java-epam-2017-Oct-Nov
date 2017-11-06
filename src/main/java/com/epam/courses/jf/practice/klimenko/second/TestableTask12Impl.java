a53356f6a514109d014f2134f79e2df58eaf627f
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-07 01:12:20 +0300

    Implemented task2-12

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask12Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask12Impl.java
@@ -2,11 +2,24 @@ package com.epam.courses.jf.practice.klimenko.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask12;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class TestableTask12Impl implements ITestableTask12 {
     @Override
     public List<Integer> transform(List<Integer> integers, int value) {
-        return null;
+        int middle = 0;
+        for( int i = 0; i < integers.size(); ++i ) {
+            if( integers.get(i) <= value ) {
+                if( i != middle ) {
+                    Integer tmp = integers.get(middle);
+                    integers.set(middle, integers.get(i));
+                    integers.set(i, tmp);
+                }
+                ++middle;
+            }
+        }
+        return integers;
     }
 }
