70fed64580e8b9979e970654bf9f45baa3acb032
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-06 23:42:50 +0300

    Implemented task2-3

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask3Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask3Impl.java
@@ -2,12 +2,23 @@ package com.epam.courses.jf.practice.klimenko.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask3;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TestableTask3Impl implements ITestableTask3 {
     @Override
     public List<String> sortPoems(Set<IPoem> poems, String author) {
-        return null;
+        List<String> list = new ArrayList<>();
+        for (IPoem p : poems
+                .stream()
+                .filter((IPoem p) -> p.getAuthor().equals(author))
+                .collect(Collectors.toList())) {
+            list.addAll(p.getLines());
+        }
+        list.sort(Comparator.comparing(s->s.length()));
+        return list;
     }
 }
