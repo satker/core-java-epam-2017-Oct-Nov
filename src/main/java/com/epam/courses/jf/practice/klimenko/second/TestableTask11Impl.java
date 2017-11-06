a640555e5df74ade20f7e63dc41eb49eed1ef5d5
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-07 01:03:51 +0300

    Implemented task2-11

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask11Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask11Impl.java
@@ -8,11 +8,21 @@ import java.util.LinkedList;
 public class TestableTask11Impl implements ITestableTask11 {
     @Override
     public String emulate(ArrayList<String> peoples) {
-        return null;
+        while (peoples.size() > 1) {
+            for (int i = 0; i < peoples.size(); ++i) {
+                peoples.remove(i);
+            }
+        }
+        return peoples.get(0);
     }
 
     @Override
     public String emulate(LinkedList<String> peoples) {
-        return null;
+        while (peoples.size() > 1) {
+            for (int i = 0; i < peoples.size(); ++i) {
+                peoples.remove(i);
+            }
+        }
+        return peoples.get(0);
     }
 }
