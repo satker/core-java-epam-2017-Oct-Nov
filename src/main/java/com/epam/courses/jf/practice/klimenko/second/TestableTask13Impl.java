77f142d83c22fc959166885977dcc6c6382265b2
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-07 01:40:03 +0300

    Implemented task2-13

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask13Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask13Impl.java
@@ -5,6 +5,30 @@ import com.epam.courses.jf.practice.common.second.ITestableTask13;
 public class TestableTask13Impl implements ITestableTask13 {
     @Override
     public AbstractGraph createGraph(int numberNodes) {
-        return null;
+        return new GraphImpl(numberNodes);
+    }
+
+    private class GraphImpl extends AbstractGraph {
+        private Boolean[][] edges;
+
+        GraphImpl(int numberNodes) {
+            super(numberNodes);
+            edges = new Boolean[numberNodes][numberNodes];
+        }
+
+        @Override
+        public void addEdge(int first, int second) {
+            edges[first][second] = edges[second][first] = true;
+        }
+
+        @Override
+        public void removeEdge(int first, int second) {
+            edges[first][second] = edges[second][first] = false;
+        }
+
+        @Override
+        public boolean isExistEdge(int first, int second) {
+            return edges[first][second];
+        }
     }
 }
