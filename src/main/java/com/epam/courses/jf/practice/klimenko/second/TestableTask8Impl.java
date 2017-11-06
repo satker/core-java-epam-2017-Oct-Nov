e6921c34981abe96776f1c706670c0f56a8ea50d
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-07 00:48:12 +0300

    Implemented task2-8

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask8Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask8Impl.java
@@ -2,9 +2,50 @@ package com.epam.courses.jf.practice.klimenko.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask8;
 
+import java.util.Stack;
+
 public class TestableTask8Impl implements ITestableTask8 {
+    private static boolean isOpening(Character c) {
+        return c == '(' || c == '[' || c == '{';
+    }
+
+    private static boolean isClosing(Character c) {
+        return c == ')' || c == ']' || c == '}';
+    }
+
+    private static Character flip(Character c) {
+        switch (c) {
+            case '(':
+                return ')';
+            case ')':
+                return '(';
+            case '[':
+                return ']';
+            case ']':
+                return '[';
+            case '{':
+                return '}';
+            case '}':
+                return '{';
+        }
+        return '\0';
+    }
+
     @Override
     public boolean isNormalBrackets(String string) {
-        return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < string.length(); ++i) {
+            Character c = string.charAt(i);
+            if (isOpening(c)) {
+                stack.add(c);
+            }
+            if (isClosing(c)) {
+                if (stack.empty() || stack.lastElement() != flip(c)) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.empty();
     }
 }
