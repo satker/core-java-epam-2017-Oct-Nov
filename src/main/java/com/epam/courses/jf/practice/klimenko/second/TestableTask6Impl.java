abb633e3a57216120bf6ea18de5415acbb0954b3
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-07 00:05:55 +0300

    Implemented task2-6

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask6Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask6Impl.java
@@ -3,10 +3,22 @@ package com.epam.courses.jf.practice.klimenko.second;
 import com.epam.courses.jf.practice.common.second.ITestableTask6;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class TestableTask6Impl implements ITestableTask6 {
     @Override
     public HashMap<Integer, Integer> addPolynomials(HashMap<Integer, Integer> first, HashMap<Integer, Integer> second) {
-        return null;
+        HashMap<Integer, Integer> ret = new HashMap<>();
+        ret.putAll(first);
+
+        for (Map.Entry<Integer, Integer> powerValuePair : second.entrySet()) {
+            Integer power = powerValuePair.getKey();
+            Integer value = powerValuePair.getValue();
+
+            ret.putIfAbsent(power, 0);
+            ret.put(power, ret.get(power) + value);
+        }
+
+        return ret;
     }
 }
