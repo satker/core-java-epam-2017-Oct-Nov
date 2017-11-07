aaa992d38bf03a42684feafc659198831add87f7
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-07 09:39:52 +0300

    Update solution for task 2.5

--- src/main/java/com/epam/courses/jf/practice/sskovalevskiy/second/ITestableTask5Impl.java
+++ src/main/java/com/epam/courses/jf/practice/sskovalevskiy/second/ITestableTask5Impl.java
@@ -2,14 +2,26 @@ package com.epam.courses.jf.practice.sskovalevskiy.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask5;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by asus on 01.11.2017.
+ * Решено, как описано в  http://yun.moluch.ru/archive/5/287/
  */
-public class ITestableTask5Impl implements ITestableTask5{
+public class ITestableTask5Impl implements ITestableTask5 {
+
     @Override
     public double calcResistance(List<IMeasurement> measurements) {
-        return 0;
+
+        double UI = 0;
+        double I2 = 0;
+
+        for (IMeasurement measurement : measurements) {
+            UI += measurement.getVoltage() * measurement.getCurrent();
+            I2 += Math.pow(measurement.getCurrent(), 2);
+        }
+
+        return UI/I2;
     }
 }
