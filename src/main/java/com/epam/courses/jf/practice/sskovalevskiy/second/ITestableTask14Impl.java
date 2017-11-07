4474733fe2e3a928b1692fcdc89339ae47882a03
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-07 10:57:43 +0300

    Update solution for task 2.14

--- src/main/java/com/epam/courses/jf/practice/sskovalevskiy/second/ITestableTask14Impl.java
+++ src/main/java/com/epam/courses/jf/practice/sskovalevskiy/second/ITestableTask14Impl.java
@@ -2,12 +2,28 @@ package com.epam.courses.jf.practice.sskovalevskiy.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask14;
 
+import java.util.AbstractCollection;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Comparator;
+
 /**
  * Created by asus on 01.11.2017.
  */
 public class ITestableTask14Impl implements ITestableTask14 {
     @Override
     public <T extends Number> INumberCollection<T> createCollection(Class<T> required) {
-        return null;
+        return new NumberCollection<T>();
+    }
+
+    class NumberCollection<T extends Number> extends ArrayList<T> implements ITestableTask14.INumberCollection<T> {
+
+        @Override
+        public T nearest(T value) {
+
+            return stream()
+                    .min(Comparator.comparingDouble(o -> Math.abs(o.doubleValue() - value.doubleValue())))
+                    .get();
+        }
     }
 }
