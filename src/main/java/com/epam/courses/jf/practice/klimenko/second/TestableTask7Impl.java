01a29aa810f1fe7f46703fa8091e71cbb64946a2
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-07 21:58:28 +0300

    Fixed task2-7

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask7Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask7Impl.java
@@ -2,18 +2,28 @@ package com.epam.courses.jf.practice.klimenko.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask7;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestableTask7Impl implements ITestableTask7 {
     @Override
     public List<Integer> multiplyPolynomials(List<Integer> first, List<Integer> second) {
-        Integer[] ret = new Integer[first.size() + second.size()];
-        for(int firstPower = 0; firstPower < first.size(); ++firstPower) {
-            for(int secondPower = 0; secondPower < second.size(); ++secondPower) {
-                ret[firstPower + secondPower] += first.get(firstPower) * second.get(secondPower);
+        List<Integer> ret = new ArrayList<>();
+        for (int firstPower = 0; firstPower < first.size(); ++firstPower) {
+            for (int secondPower = 0; secondPower < second.size(); ++secondPower) {
+                int power = firstPower + secondPower;
+                int value = first.get(firstPower) * second.get(secondPower);
+                if (value != 0) {
+                    while (ret.size() <= power) {
+                        ret.add(0);
+                    }
+                    ret.set(power, ret.get(power) + value);
+                }
             }
         }
-        return Arrays.asList(ret);
+        if (ret.isEmpty()) {
+            ret.add(0);
+        }
+        return ret;
     }
 }
