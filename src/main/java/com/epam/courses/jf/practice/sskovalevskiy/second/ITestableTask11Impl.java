a214b0e9993f3e3b8cadb88f8dacc27c9cc3d870
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-07 11:33:45 +0300

    Update solution for task 2.11

--- src/main/java/com/epam/courses/jf/practice/sskovalevskiy/second/ITestableTask11Impl.java
+++ src/main/java/com/epam/courses/jf/practice/sskovalevskiy/second/ITestableTask11Impl.java
@@ -3,6 +3,7 @@ package com.epam.courses.jf.practice.sskovalevskiy.second;
 import com.epam.courses.jf.practice.common.second.ITestableTask11;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
@@ -11,11 +12,31 @@ import java.util.LinkedList;
 public class ITestableTask11Impl implements ITestableTask11 {
     @Override
     public String emulate(ArrayList<String> peoples) {
-        return null;
+        ArrayList<String> persons = new ArrayList<>(peoples);
+        while (persons.size() > 1) {
+            Iterator iterator = persons.iterator();
+            for (int i = 0; iterator.hasNext(); i++, iterator.next()) {
+                if (i % 2 == 1) {
+                    i = 0;
+                    iterator.remove();
+                }
+            }
+        }
+        return persons.get(0);
     }
 
     @Override
     public String emulate(LinkedList<String> peoples) {
-        return null;
+        LinkedList<String> persons = new LinkedList<>(peoples);
+        while (persons.size() > 1) {
+            Iterator iterator = persons.iterator();
+            for (int i = 0; iterator.hasNext(); i++, iterator.next()) {
+                if (i % 2 == 1) {
+                    i = 0;
+                    iterator.remove();
+                }
+            }
+        }
+        return persons.get(0);
     }
 }
