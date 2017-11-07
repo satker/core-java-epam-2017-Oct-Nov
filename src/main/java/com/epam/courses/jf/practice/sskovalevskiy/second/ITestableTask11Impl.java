eb8c6868d504c0b2e9693e814f99abe6aa171904
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-07 14:27:25 +0300

    Update solution for task 2.11

--- src/main/java/com/epam/courses/jf/practice/sskovalevskiy/second/ITestableTask11Impl.java
+++ src/main/java/com/epam/courses/jf/practice/sskovalevskiy/second/ITestableTask11Impl.java
@@ -13,11 +13,13 @@ public class ITestableTask11Impl implements ITestableTask11 {
     @Override
     public String emulate(ArrayList<String> peoples) {
         ArrayList<String> persons = new ArrayList<>(peoples);
+        int i = 0;
         while (persons.size() > 1) {
             Iterator iterator = persons.iterator();
-            for (int i = 0; iterator.hasNext(); i++, iterator.next()) {
+
+            for (;iterator.hasNext(); i++, iterator.next()) {
                 if (i % 2 == 1) {
-                    i = 0;
+                    if (iterator.hasNext()) i = 0;
                     iterator.remove();
                 }
             }
@@ -28,11 +30,12 @@ public class ITestableTask11Impl implements ITestableTask11 {
     @Override
     public String emulate(LinkedList<String> peoples) {
         LinkedList<String> persons = new LinkedList<>(peoples);
+        int i = 0;
         while (persons.size() > 1) {
             Iterator iterator = persons.iterator();
-            for (int i = 0; iterator.hasNext(); i++, iterator.next()) {
+            for (; iterator.hasNext(); i++, iterator.next()) {
                 if (i % 2 == 1) {
-                    i = 0;
+                    if (iterator.hasNext()) i = 0;
                     iterator.remove();
                 }
             }
