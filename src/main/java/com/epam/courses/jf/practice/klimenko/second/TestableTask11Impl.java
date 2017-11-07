45fd3a54006e7f9288b6bb8a6be8b9b8af110697
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-07 22:16:45 +0300

    Updated solution for linked list in task2-11

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask11Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask11Impl.java
@@ -3,6 +3,7 @@ package com.epam.courses.jf.practice.klimenko.second;
 import com.epam.courses.jf.practice.common.second.ITestableTask11;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class TestableTask11Impl implements ITestableTask11 {
@@ -18,10 +19,16 @@ public class TestableTask11Impl implements ITestableTask11 {
 
     @Override
     public String emulate(LinkedList<String> peoples) {
+        LinkedList<String> toErase = new LinkedList<>();
         while (peoples.size() > 1) {
-            for (int i = 0; i < peoples.size(); ++i) {
-                peoples.remove(i);
+            toErase.clear();
+            for (Iterator<String> it = peoples.iterator(); it.hasNext();) {
+                toErase.add(it.next());
+                if(it.hasNext()) {
+                    it.next();
+                }
             }
+            peoples.removeAll(toErase);
         }
         return peoples.get(0);
     }
