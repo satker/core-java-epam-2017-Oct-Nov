309ed7edce9231f6a70b1e98892c55df4e7a761d
klm-1 <klimenko.v.2.0@gmail.com>
2017-11-07 22:33:01 +0300

    Fixed task2-12

--- src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask12Impl.java
+++ src/main/java/com/epam/courses/jf/practice/klimenko/second/TestableTask12Impl.java
@@ -5,17 +5,26 @@ import com.epam.courses.jf.practice.common.second.ITestableTask12;
 import java.util.List;
 
 public class TestableTask12Impl implements ITestableTask12 {
+    private void swap(List<Integer> list, int i, int j) {
+        if( i != j ) {
+            Integer tmp = list.get(i);
+            list.set(i, list.get(j));
+            list.set(j, tmp);
+        }
+    }
+
     @Override
     public List<Integer> transform(List<Integer> integers, int value) {
-        int middle = 0;
+        int less = 0, equals = 0;
         for (int i = 0; i < integers.size(); ++i) {
-            if (integers.get(i) <= value) {
-                if (i != middle) {
-                    Integer tmp = integers.get(middle);
-                    integers.set(middle, integers.get(i));
-                    integers.set(i, tmp);
-                }
-                ++middle;
+            if (integers.get(i) < value) {
+                swap(integers, less, equals);
+                swap(integers, less, i);
+                ++equals;
+                ++less;
+            } else if(integers.get(i) == value) {
+                swap(integers, equals, i);
+                ++equals;
             }
         }
         return integers;
