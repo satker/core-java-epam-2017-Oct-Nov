a9a8fde38e18d38fba295d1f2ab4dfe039733991
nbikbaev <niaz.bikbaev@gmail.com>
2017-11-09 02:50:52 +0300

    Task11 test #1

--- src/main/java/com/epam/courses/jf/practice/nbikbaev/first/Solver.java
+++ src/main/java/com/epam/courses/jf/practice/nbikbaev/first/Solver.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -295,7 +296,48 @@ public class Solver implements ISolver {
 
     @Override
     public void task11() {
-
+        Scanner scanner = new Scanner(System.in);
+        int month = scanner.nextInt();
+        switch (month) {
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+            default:
+                System.out.println("INCORRECT INPUT DATA");
+        }
     }
 
     @Override
