e8e21d36fb3ef4b0bbc97f2c23b5a20ff4316eef
vakhonin <isvakhonin@yandex.ru>
2017-11-08 19:09:43 +0300

    add_task_2

--- src/main/java/com/epam/courses/jf/practice/vakhonin/Solver.java
+++ src/main/java/com/epam/courses/jf/practice/vakhonin/Solver.java
@@ -2,7 +2,7 @@ package com.epam.courses.jf.practice.vakhonin;
 
 import com.epam.courses.jf.practice.common.first.ISolver;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by igorvahonin on 03.11.17.
@@ -45,5 +45,35 @@ public class Solver implements ISolver {
         maxString = strings[numberOfLongestString];
         System.out.printf("MIN (%d): \"%s\"%n", minLength, minString);
         System.out.printf("MAX (%d): \"%s\"%n", maxLength, maxString);
+    }
+
+    public void task2() {
+        int numberOfStrings, length;
+        Map<Integer, ArrayList<String>> stringsMap;
+        Scanner in;
+
+        in = new Scanner(System.in);
+        numberOfStrings = in.nextInt();
+        String[] strings = new String[numberOfStrings];
+        in = new Scanner(System.in);
+        for (int j = 0; j < numberOfStrings; j++) {
+            strings[j] = in.nextLine();
+        }
+        stringsMap = new TreeMap();
+        for (String string : strings) {
+            length = string.length();
+            if (!stringsMap.containsKey(length)) {
+                stringsMap.put(length, new ArrayList());
+            }
+            stringsMap.get(length).add(string);
+        }
+
+        for (Map.Entry<Integer, ArrayList<String>> entry : stringsMap.entrySet()) {
+            List<String> stringListWithSameLength = entry.getValue();
+            stringListWithSameLength.sort(String::compareToIgnoreCase);
+            for (String s : stringListWithSameLength) {
+                System.out.printf("(%d): \"%s\"%n", entry.getKey(), s);
+            }
+        }
     }
 }
