63ecf3cc01715cea320915742480ebac07000016
AleksandrKriukov <alexanderkry@gmail.com>
2017-11-09 19:01:08 +0300

    task1.1 slightly modified
    task1.2 done

--- src/main/java/com/epam/courses/jf/practice/akriukov/first/Solver.java
+++ src/main/java/com/epam/courses/jf/practice/akriukov/first/Solver.java
@@ -5,16 +5,16 @@ import com.epam.courses.jf.practice.common.first.ISolver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Solver implements ISolver{
-
 
     @Override
     public void task1() {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            int numberOfLines = 0;
-            numberOfLines = Integer.parseInt(reader.readLine());
+            int numberOfLines = Integer.parseInt(reader.readLine());
             String[] lines = new String[numberOfLines];
 
             for (int i = 0; i < numberOfLines; i++) {
@@ -41,6 +41,39 @@ public class Solver implements ISolver{
             System.out.printf("MAX (%d): \"%s\"%n", maxLength, maxString);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void task2() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            int numberOfLines = Integer.parseInt(reader.readLine());
+            String[] lines = new String[numberOfLines];
+
+            for (int i = 0; i < numberOfLines; i++) {
+                lines[i] = reader.readLine();
+            }
+            Arrays.sort(lines, new compByLength());
+            int length;
+            String string;
+            for (int i = 0; i < numberOfLines; i++) {
+                length = lines[i].length();
+                string = lines[i];
+                System.out.printf("(%d): \"%s\"%n", length, string);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class compByLength implements Comparator<String> { //comparator for task2
+        public int compare(String o1, String o2) {
+            if (o1.length() != o2.length()) {
+                return Integer.compare(o1.length(), o2.length()); //firstly compare length
+            } else {
+                return String.CASE_INSENSITIVE_ORDER.compare(o1, o2); //compare symbols, if same length
+            }
         }
     }
 }
