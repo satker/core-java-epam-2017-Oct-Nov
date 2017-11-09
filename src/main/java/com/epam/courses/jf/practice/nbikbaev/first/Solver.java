f910926a8f21f381b32ce2c8555f465d17a17c18
nbikbaev <niaz.bikbaev@gmail.com>
2017-11-09 03:04:52 +0300

    Task11 test#3

--- src/main/java/com/epam/courses/jf/practice/nbikbaev/first/Solver.java
+++ src/main/java/com/epam/courses/jf/practice/nbikbaev/first/Solver.java
@@ -297,53 +297,56 @@ public class Solver implements ISolver {
 
     @Override
     public void task11() {
-        Scanner scanner = new Scanner(System.in);
-        String dataIn = scanner.nextLine();
-        int month = 0;
-        try {
-            month = Integer.valueOf(dataIn);
-        } catch (RuntimeException e) {
-            System.out.println("INCORRECT INPUT DATA");
-        }
-        switch (month) {
-            case 1:
-                System.out.println("January");
-                break;
-            case 2:
-                System.out.println("February");
-                break;
-            case 3:
-                System.out.println("March");
-                break;
-            case 4:
-                System.out.println("April");
-                break;
-            case 5:
-                System.out.println("May");
-                break;
-            case 6:
-                System.out.println("June");
-                break;
-            case 7:
-                System.out.println("July");
-                break;
-            case 8:
-                System.out.println("August");
-                break;
-            case 9:
-                System.out.println("September");
-                break;
-            case 10:
-                System.out.println("October");
-                break;
-            case 11:
-                System.out.println("November");
-                break;
-            case 12:
-                System.out.println("December");
-                break;
-            default:
+        try (Scanner scanner = new Scanner(System.in);) {
+            String dataIn = scanner.nextLine();
+            int month = 0;
+            try {
+                month = Integer.valueOf(dataIn);
+            } catch (RuntimeException e) {
                 System.out.println("INCORRECT INPUT DATA");
+                return;
+            }
+
+            switch (month) {
+                case 1:
+                    System.out.println("January");
+                    break;
+                case 2:
+                    System.out.println("February");
+                    break;
+                case 3:
+                    System.out.println("March");
+                    break;
+                case 4:
+                    System.out.println("April");
+                    break;
+                case 5:
+                    System.out.println("May");
+                    break;
+                case 6:
+                    System.out.println("June");
+                    break;
+                case 7:
+                    System.out.println("July");
+                    break;
+                case 8:
+                    System.out.println("August");
+                    break;
+                case 9:
+                    System.out.println("September");
+                    break;
+                case 10:
+                    System.out.println("October");
+                    break;
+                case 11:
+                    System.out.println("November");
+                    break;
+                case 12:
+                    System.out.println("December");
+                    break;
+                default:
+                    System.out.println("INCORRECT INPUT DATA");
+            }
         }
     }
 
