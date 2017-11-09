19a90903f69cc90e97cb71030e12da4c42a2a6a5
Herman Kryzhik <fraarson@gmail.com>
2017-11-09 21:02:21 +0300

    task 10 fixed

--- src/main/java/com/epam/courses/jf/practice/hkryzhik/first/Solver.java
+++ src/main/java/com/epam/courses/jf/practice/hkryzhik/first/Solver.java
@@ -373,11 +373,11 @@ public class Solver implements ISolver {
             x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4*a*c))/(2 * a);
             x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4*a*c))/(2 * a);
             if(x1 == 0) {
-                System.out.printf("Two solutions: %.1f, %.0f\n", x2, x1);
+                System.out.printf("Two solutions: %.0f, %.1f\n", x1, x2);
             }else if (x2 == 0){
-                System.out.printf("Two solutions: %.0f, %.1f\n", x2, x1);
+                System.out.printf("Two solutions: %.1f, %.0f\n", x1, x2);
             }else{
-                System.out.printf("Two solutions: %.1f, %.1f\n", x2, x1);
+                System.out.printf("Two solutions: %.1f, %.1f\n", x1, x2);
             }
         }
         if(d == 0){
