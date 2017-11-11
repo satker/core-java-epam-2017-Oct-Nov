a524299587751f7535b99d787fff5e758e919d1c
asunyaev <a.a.syunyaev@gmail.com>
2017-11-11 13:42:51 +0300

    task 2.2 fixed

--- src/main/java/com/epam/courses/jf/practice/asunyaev/second/Task2.java
+++ src/main/java/com/epam/courses/jf/practice/asunyaev/second/Task2.java
@@ -3,14 +3,22 @@ package com.epam.courses.jf.practice.asunyaev.second;
 import com.epam.courses.jf.practice.common.second.ITestableTask2;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Task2 implements ITestableTask2 {
     @Override
     public Set<File> getFiles(File directory) {
-        Set<File> files = new HashSet<>(Arrays.asList(directory.listFiles()));
+        Set<File> files = new HashSet<>();
+        Queue<File> queue = new LinkedList<>(Arrays.asList(directory.listFiles()));
+
+        while (!queue.isEmpty()) {
+            File currentFile = queue.remove();
+            files.add(currentFile);
+            if (currentFile.isDirectory()) {
+                Collections.addAll(queue, currentFile.listFiles());
+            }
+        }
+
         return files;
     }
 }
