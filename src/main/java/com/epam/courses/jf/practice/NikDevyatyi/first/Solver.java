b5a69949a24ab8a1aaecbb23851f9dcead16e0c0
ndevyatiy <hvoltage9@yandex.ru>
2017-11-13 03:33:54 +0300

    1 добавлено решение task17() 2 добавлен метод getMatrixWithoutRowAndCol() 3 добавлен метод matrixDet()

--- src/main/java/com/epam/courses/jf/practice/NikDevyatyi/first/Solver.java
+++ src/main/java/com/epam/courses/jf/practice/NikDevyatyi/first/Solver.java
@@ -531,5 +531,82 @@ public class Solver implements ISolver {
         }
 
     }
+    /***
+     *
+     * @param matrix
+     * @param size
+     * @param row
+     * @param col
+     * @param newMatrix
+     * @return int[][]
+     * <p>getMatrixWithoutRowAndCol method to calculate minors in matrix</p>
+     */
+    private int[][] getMatrixWithoutRowAndCol(int [][]matrix, int size, int row, int col, int [][]newMatrix) {
+        int offsetRow = 0; //Смещение индекса строки в матрице
+        int offsetCol = 0; //Смещение индекса столбца в матрице
+        for(int i = 0; i < size-1; i++) {
+            //Пропустить row-ую строку
+            if(i == row) {
+                offsetRow = 1; //Как только встретили строку, которую надо пропустить, делаем смещение для исходной матрицы
+            }
+
+            offsetCol = 0; //Обнулить смещение столбца
+            for(int j = 0; j < size-1; j++) {
+                if(j == col) {
+                    offsetCol = 1; //Встретили нужный столбец, проускаем его смещением
+                }
+
+                newMatrix[i][j] = matrix[i + offsetRow][j + offsetCol];
+            }
+
+        }
+        return newMatrix;
+    }
+    /*checked tasks1-17*/
+    @Override
+    public void task17() {
+        Scanner scanner =  new Scanner(System.in);
+        int[][] matrix = readMatrix(scanner);
+        int size = matrix.length;
+        System.out.println(matrixDet(matrix,size));
+    }
+
+    /***
+     *
+     * @param matrix
+     * @param size
+     * @return int
+     * <p>matrixDet calculate determinant of matrix</p>
+     */
+    private int matrixDet(int [][]matrix,int size) {
+        int det = 0;
+        int degree = 1;
+        if(size == 1) {
+            return matrix[0][0];
+        }
+        else if(size == 2) {
+            return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0];
+        }
+        else {
+            int [][]newMatrix = new int[size-1][size-1];
+            for(int i = 0; i < size-1; i++) {
+                newMatrix[i] = new int[size-1];
+            }
+
+            for(int j = 0; j < size; j++) {
+                getMatrixWithoutRowAndCol(matrix, size, 0, j, newMatrix);
+                det = det + (degree * matrix[0][j]* matrixDet(newMatrix, size-1));
+
+                degree = -degree;
+            }
+
+            for(int i = 0; i < size-1; i++) {
+
+            }
+
+        }
+
+        return det;
+    }
 
 }
