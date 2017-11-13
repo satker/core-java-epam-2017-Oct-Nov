e682819c6f54e2396a8b1eefa9cd96c6ee6344bd
asunyaev <a.a.syunyaev@gmail.com>
2017-11-13 11:22:28 +0300

    task 2.3 implemented

---
+++ src/main/java/com/epam/courses/jf/practice/asunyaev/second/Task3.java
@@ -0,0 +1,26 @@
+package com.epam.courses.jf.practice.asunyaev.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask3;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+public class Task3 implements ITestableTask3 {
+    public List<String> sortPoems(Set<IPoem> poems, String author) {
+        List<String> lines = new ArrayList<>();
+        for (IPoem poem : poems) {
+           if (poem.getAuthor().equals(author)) {
+               lines.addAll(poem.getLines());
+           }
+       }
+       lines.sort(new Comparator<String>() {
+           @Override
+           public int compare(String o1, String o2) {
+               return (Integer.compare(o1.length(), o2.length()));
+           }
+       });
+       return lines;
+    }
+}
