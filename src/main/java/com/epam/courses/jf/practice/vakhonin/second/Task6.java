a8659c7eb7e741271a03bbfd92091fbee443a949
vakhonin <isvakhonin@yandex.ru>
2017-11-13 12:04:46 +0300

    add_only_task2_6

--- src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task6.java
+++ src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task6.java
@@ -1,7 +1,34 @@
 package com.epam.courses.jf.practice.vakhonin.second;
 
+import com.epam.courses.jf.practice.common.second.ITestableTask6;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by igorvahonin on 13.11.17.
  */
-public class Task6 {
+public class Task6 implements ITestableTask6{
+
+    @Override
+    public HashMap<Integer, Integer> addPolynomials(HashMap<Integer, Integer> first, HashMap<Integer, Integer> second) {
+        HashMap<Integer, Integer> sumSet = new HashMap<>(first);
+
+        Integer value1, value2, sum;
+
+        for(Integer key: second.keySet()){
+            value2 = second.get(key);
+            if(first.containsKey(key)){
+                value1 = first.get(key);
+                sum = value1 + value2;
+                sumSet.put(key, sum);
+            }
+            else{
+                sumSet.put(key, value2);
+            }
+        }
+        
+        return sumSet;
+    }
+
 }
