8ce2c647b0c178bcea05123e147b8b2dd852345c
gulyaev13 <gulyaev13@gmail.com>
2017-11-13 20:38:12 +0300

    Add tasks 2.7, 2.8, 2.9, 2.10 to TaskStorage

--- src/main/java/com/epam/courses/jf/practice/igulyaev/second/TaskStorage.java
+++ src/main/java/com/epam/courses/jf/practice/igulyaev/second/TaskStorage.java
@@ -16,6 +16,10 @@ public class TaskStorage implements ITaskStorage {
         taskMap.put(ITestableTask3.class.getSimpleName(), new TestableTask3());
         taskMap.put(ITestableTask4.class.getSimpleName(), new TestableTask4());
         taskMap.put(ITestableTask6.class.getSimpleName(), new TestableTask6());
+        taskMap.put(ITestableTask7.class.getSimpleName(), new TestableTask7());
+        taskMap.put(ITestableTask8.class.getSimpleName(), new TestableTask8());
+        taskMap.put(ITestableTask9.class.getSimpleName(), new TestableTask9());
+        taskMap.put(ITestableTask10.class.getSimpleName(), new TestableTask10());
     }
 
     @Override
