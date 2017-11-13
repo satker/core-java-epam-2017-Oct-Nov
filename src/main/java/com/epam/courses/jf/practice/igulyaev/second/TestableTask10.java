1e87f48643da0fe911125e813f091ac46dc73d1b
gulyaev13 <gulyaev13@gmail.com>
2017-11-13 19:55:44 +0300

    Task 2.10 implemented

---
+++ src/main/java/com/epam/courses/jf/practice/igulyaev/second/TestableTask10.java
@@ -0,0 +1,33 @@
+package com.epam.courses.jf.practice.igulyaev.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask10;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestableTask10 implements ITestableTask10 {
+    @Override
+    public HashMap<String, Integer> countNumberWords(File input) {
+        Map<String, Integer> map = new HashMap<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(input))) {
+            for(String str = bufferedReader.readLine(); str != null; str = bufferedReader.readLine()){
+                Arrays.stream(str.split(" ")).forEach(s -> {
+                    Integer count = map.get(s);
+                    if(count == null){
+                        count = 1;
+                    }
+                    map.put(s, count);
+                }
+                );
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return (HashMap<String, Integer>)map;
+    }
+}
