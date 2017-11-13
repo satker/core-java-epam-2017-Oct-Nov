b4f481decfd6718857cfeb7d2009a6ee76a61c14
vakhonin <isvakhonin@yandex.ru>
2017-11-13 12:11:03 +0300

    add_only_task2_7

--- src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task7.java
+++ src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task7.java
@@ -1,7 +1,49 @@
 package com.epam.courses.jf.practice.vakhonin.second;
 
+import com.epam.courses.jf.practice.common.second.ITestableTask7;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by igorvahonin on 13.11.17.
  */
-public class Task7 {
+public class Task7 implements ITestableTask7{
+
+    @Override
+    public List<Integer> multiplyPolynomials(List<Integer> first, List<Integer> second){
+
+        List<Integer> result = new ArrayList<>();
+
+        int size1 = first.size();
+        int size2 = second.size();
+        int val1, val2;
+
+        while(first.get(size1-1) == 0){
+            size1--;
+        }
+
+        while(second.get(size2-1) == 0){
+            size2--;
+        }
+
+
+
+
+        for(int j = 0; j < (size1 + size2 - 1); j++){
+            result.add(0);
+        }
+
+        for(int j = 0; j < size1; j++){
+            for(int k = 0; k < size2; k++){
+                val1 = first.get(j);
+                val2 = second.get(k);
+//                System.out.println(j+k);
+                result.set(j+k, val1*val2 + result.get(j+k));
+//                System.out.println(result + "");
+            }
+        }
+        return result;
+    }
+
 }
