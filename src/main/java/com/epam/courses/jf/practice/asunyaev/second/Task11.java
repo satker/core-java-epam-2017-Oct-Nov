ed17f71a2fb31cfc5f214e68bb8940e173b81221
asunyaev <a.a.syunyaev@gmail.com>
2017-11-13 20:46:38 +0300

    task 2.11 fixed

--- src/main/java/com/epam/courses/jf/practice/asunyaev/second/Task11.java
+++ src/main/java/com/epam/courses/jf/practice/asunyaev/second/Task11.java
@@ -3,6 +3,7 @@ package com.epam.courses.jf.practice.asunyaev.second;
 import com.epam.courses.jf.practice.common.second.ITestableTask11;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Task11 implements ITestableTask11 {
@@ -22,14 +23,15 @@ public class Task11 implements ITestableTask11 {
     }
 
     public String emulate(LinkedList<String> peoples) {
-        int i = 0;
-        while(peoples.size() > 1) {
-            peoples.remove(i);
-            i++;
-            if (i == peoples.size()) {
-                i = 0;
-            } else if (i > peoples.size()) {
-                i = 1;
+        boolean removeModeON = true;
+        while (peoples.size() > 1) {
+            Iterator<String> iterator = peoples.iterator();
+            while (iterator.hasNext()) {
+                iterator.next();
+                if (removeModeON) {
+                    iterator.remove();
+                }
+                removeModeON = !removeModeON;
             }
         }
         return peoples.get(0);
