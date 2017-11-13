c57cec60d63e8d72507a1ffcdbd99a679692b001
gulyaev13 <gulyaev13@gmail.com>
2017-11-13 18:28:09 +0300

    Task 2.8 implemented

---
+++ src/main/java/com/epam/courses/jf/practice/igulyaev/second/TestableTask8.java
@@ -0,0 +1,37 @@
+package com.epam.courses.jf.practice.igulyaev.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask8;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class TestableTask8 implements ITestableTask8 {
+    @Override
+    public boolean isNormalBrackets(String string) {
+        Deque<Character> stack = new ArrayDeque<>();
+        Matcher matcher = Pattern.compile("([\\[{(])|([\\]})])").matcher(string);
+        while (matcher.find()){
+            Stream.of(matcher.group(1))
+                    .filter(Objects::nonNull)
+                    .forEach(s -> stack.push(s.charAt(0)));
+            String second = matcher.group(2);
+            if(second != null && !stack.isEmpty() && stack.pop() != pairBracket(second.charAt(0))){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+    private char pairBracket(char ch){
+        switch (ch){
+            case '}':
+                return '{';
+            case ')':
+                return '(';
+            case ']':
+                return '[';
+        }
+        return 0;
+    }
+}
