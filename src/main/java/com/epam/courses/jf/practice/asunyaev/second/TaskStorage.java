301927e86d04624d3a526b6325778fa1f263e8a4
asunyaev <a.a.syunyaev@gmail.com>
2017-11-15 21:15:43 +0300

    TaskStorage refactored

--- src/main/java/com/epam/courses/jf/practice/asunyaev/second/TaskStorage.java
+++ src/main/java/com/epam/courses/jf/practice/asunyaev/second/TaskStorage.java
@@ -2,44 +2,57 @@ package com.epam.courses.jf.practice.asunyaev.second;
 
 import com.epam.courses.jf.practice.common.second.ITaskStorage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TaskStorage implements ITaskStorage {
 
     public <T extends ITestableTask> T getSolver(Class<T> taskInterface) {
-        String taskInterfaceName = taskInterface.getSimpleName();
-
-        switch(taskInterfaceName) {
-            case "ITestableTask1":
-                return (T) new Task1();
-            case "ITestableTask2":
-                return (T) new Task2();
-            case "ITestableTask3":
-                return (T) new Task3();
-            case "ITestableTask4":
-                return (T) new Task4();
-            case "ITestableTask5":
-                return (T) new Task5();
-            case "ITestableTask6":
-                return (T) new Task6();
-            case "ITestableTask7":
-                return (T) new Task7();
-            case "ITestableTask8":
-                return (T) new Task8();
-            case "ITestableTask9":
-                return (T) new Task9();
-            case "ITestableTask10":
-                return (T) new Task10();
-            case "ITestableTask11":
-                return (T) new Task11();
-            case "ITestableTask12":
-                return (T) new Task12();
-            case "ITestableTask13":
-                return (T) new Task13();
-            case "ITestableTask14":
-                return (T) new Task14();
-            case "ITestableTask15":
-                return (T) new Task15();
-            default:
-                return null;
+        if (taskInterface == Task1.class) {
+            return (T) new Task1();
         }
+        if (taskInterface == Task2.class) {
+            return (T) new Task2();
+        }
+        if (taskInterface == Task3.class) {
+            return (T) new Task3();
+        }
+        if (taskInterface == Task4.class) {
+            return (T) new Task4();
+        }
+        if (taskInterface == Task5.class) {
+            return (T) new Task5();
+        }
+        if (taskInterface == Task6.class) {
+            return (T) new Task6();
+        }
+        if (taskInterface == Task7.class) {
+            return (T) new Task7();
+        }
+        if (taskInterface == Task8.class) {
+            return (T) new Task8();
+        }
+        if (taskInterface == Task9.class) {
+            return (T) new Task9();
+        }
+        if (taskInterface == Task10.class) {
+            return (T) new Task10();
+        }
+        if (taskInterface == Task11.class) {
+            return (T) new Task11();
+        }
+        if (taskInterface == Task12.class) {
+            return (T) new Task12();
+        }
+        if (taskInterface == Task13.class) {
+            return (T) new Task13();
+        }
+        if (taskInterface == Task14.class) {
+            return (T) new Task14();
+        }
+        if (taskInterface == Task15.class) {
+            return (T) new Task15();
+        }
+        return null;
     }
 }
---
+++ src/main/java/com/epam/courses/jf/practice/asunyaev/second/TestTask15.java
@@ -0,0 +1,4 @@
+package com.epam.courses.jf.practice.asunyaev.second;
+
+public class TestTask15 {
+}
