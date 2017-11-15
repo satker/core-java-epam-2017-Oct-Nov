ecd0443b94106416a5bd60c4eb714992a410fb96
student <student@EPRUPETW0557.PETERSBURG.epam.com>
2017-11-15 18:50:33 +0300

    Task 4 of part 2 implemented

---
+++ src/main/java/com/epam/courses/jf/practice/hkryzhik/second/Task4.java
@@ -0,0 +1,31 @@
+package com.epam.courses.jf.practice.hkryzhik.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask4;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Task4 implements ITestableTask4 {
+
+    @Override
+    public Set<Integer> intersection(Set<Integer> first, Set<Integer> second) {
+        if(first.containsAll(second)){
+            return first;
+        }
+        Set<Integer> resultSet = first.stream().filter(e -> second.contains(e)).collect(Collectors.toSet());
+
+        return resultSet;
+    }
+
+    @Override
+    public Set<Integer> union(Set<Integer> first, Set<Integer> second) {
+
+        if(first.containsAll(second)){
+            return first;
+        }
+
+        first.addAll(second);
+
+        return first;
+    }
+}
--- src/main/java/com/epam/courses/jf/practice/hkryzhik/second/TaskStorage.java
+++ src/main/java/com/epam/courses/jf/practice/hkryzhik/second/TaskStorage.java
@@ -10,7 +10,7 @@ public class TaskStorage implements ITaskStorage{
             case 1: return (T) new Task1();
             case 2: return (T) new Task2();
             case 3: return (T) new Task3();
-//            case "Task4": return (T) new Task4();
+            case 4: return (T) new Task4();
 //            case "Task5": return (T) new Task5();
 //            case "Task6": return (T) new Task6();
 //            case "Task7": return (T) new Task7();
