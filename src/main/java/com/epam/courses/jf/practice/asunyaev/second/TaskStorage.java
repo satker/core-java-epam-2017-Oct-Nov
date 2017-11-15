091fc4a202d508553269506fdd9a99c546bdefa7
asunyaev <a.a.syunyaev@gmail.com>
2017-11-15 21:19:57 +0300

    TaskStorage refactored

--- src/main/java/com/epam/courses/jf/practice/asunyaev/second/TaskStorage.java
+++ src/main/java/com/epam/courses/jf/practice/asunyaev/second/TaskStorage.java
@@ -1,6 +1,6 @@
 package com.epam.courses.jf.practice.asunyaev.second;
 
-import com.epam.courses.jf.practice.common.second.ITaskStorage;
+import com.epam.courses.jf.practice.common.second.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,49 +8,49 @@ import java.util.Map;
 public class TaskStorage implements ITaskStorage {
 
     public <T extends ITestableTask> T getSolver(Class<T> taskInterface) {
-        if (taskInterface == Task1.class) {
+        if (taskInterface == ITestableTask1.class) {
             return (T) new Task1();
         }
-        if (taskInterface == Task2.class) {
+        if (taskInterface == ITestableTask2.class) {
             return (T) new Task2();
         }
-        if (taskInterface == Task3.class) {
+        if (taskInterface == ITestableTask3.class) {
             return (T) new Task3();
         }
-        if (taskInterface == Task4.class) {
+        if (taskInterface == ITestableTask4.class) {
             return (T) new Task4();
         }
-        if (taskInterface == Task5.class) {
+        if (taskInterface == ITestableTask5.class) {
             return (T) new Task5();
         }
-        if (taskInterface == Task6.class) {
+        if (taskInterface == ITestableTask6.class) {
             return (T) new Task6();
         }
-        if (taskInterface == Task7.class) {
+        if (taskInterface == ITestableTask7.class) {
             return (T) new Task7();
         }
-        if (taskInterface == Task8.class) {
+        if (taskInterface == ITestableTask8.class) {
             return (T) new Task8();
         }
-        if (taskInterface == Task9.class) {
+        if (taskInterface == ITestableTask9.class) {
             return (T) new Task9();
         }
-        if (taskInterface == Task10.class) {
+        if (taskInterface == ITestableTask10.class) {
             return (T) new Task10();
         }
-        if (taskInterface == Task11.class) {
+        if (taskInterface == ITestableTask11.class) {
             return (T) new Task11();
         }
-        if (taskInterface == Task12.class) {
+        if (taskInterface == ITestableTask12.class) {
             return (T) new Task12();
         }
-        if (taskInterface == Task13.class) {
+        if (taskInterface == ITestableTask13.class) {
             return (T) new Task13();
         }
-        if (taskInterface == Task14.class) {
+        if (taskInterface == ITestableTask14.class) {
             return (T) new Task14();
         }
-        if (taskInterface == Task15.class) {
+        if (taskInterface == ITestableTask15.class) {
             return (T) new Task15();
         }
         return null;
