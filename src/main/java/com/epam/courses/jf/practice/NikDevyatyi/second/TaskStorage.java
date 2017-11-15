a59219581ba7251ba6b9d4734463e6d97ec96ac5
ndevyatiy <hvoltage9@yandex.ru>
2017-11-15 20:15:22 +0300

    1 Отредактирован TaskStorage

--- src/main/java/com/epam/courses/jf/practice/NikDevyatyi/second/TaskStorage.java
+++ src/main/java/com/epam/courses/jf/practice/NikDevyatyi/second/TaskStorage.java
@@ -5,15 +5,15 @@ import com.epam.courses.jf.practice.common.second.ITaskStorage;
 public class TaskStorage implements ITaskStorage {
     public <T extends ITestableTask> T getSolver(Class<T> taskInterface) {
         switch (taskInterface.getSimpleName()){
-            case "ITestabaleTask1":
+            case "ITestableTask1":
                 return (T) new  TestableTask1();
         }
         switch (taskInterface.getSimpleName()){
-            case "ITestabaleTask2":
+            case "ITestableTask2":
                 return (T) new  TestableTask2();
         }
         switch (taskInterface.getSimpleName()){
-            case "ITestabaleTask3":
+            case "ITestableTask3":
                 return (T) new  TestableTask3();
         }
         return null;
