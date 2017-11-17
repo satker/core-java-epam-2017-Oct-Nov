27c51af3af2b117ba46fadaf0fbc8175f1d62629
asunyaev <a.a.syunyaev@gmail.com>
2017-11-17 18:58:07 +0300

    Task19 refactored

--- src/main/java/com/epam/courses/jf/practice/asunyaev/second/Task19.java
+++ src/main/java/com/epam/courses/jf/practice/asunyaev/second/Task19.java
@@ -4,20 +4,11 @@ import com.epam.courses.jf.practice.common.second.ITestableTask19;
 
 import java.util.*;
 
-import static java.lang.Math.ceil;
-
 public class Task19 implements ITestableTask19 {
-
-    public static void main(String[] args) {
-        Task19 task = new Task19();
-        Set<ICar> cars = new HashSet<ICar>(Arrays.asList(new Car(0, 30), new Car(10, 5)));
-        int t = task.getNumberOvertaking(cars, 100, 2);
-        System.out.print(t);
-    }
 
     @Override
     public int getNumberOvertaking(Set<ICar> cars, long lengthLap, int numberLaps) {
-        int overtakingCount = 0;
+        int numberOvertaking = 0;
         ArrayList<ICar> carsList = new ArrayList<>(cars);
         carsList.sort(Comparator.comparingInt(ICar::getSpeed).reversed());
         for (int i = 0; i < carsList.size(); i++) {
@@ -32,35 +23,13 @@ public class Task19 implements ITestableTask19 {
                 for (int k = 0; k < numberLaps; k++) {
                     double t = (p2 - p1)/(v1 - v2);
                     if ((t > 0) && (t < (double) lengthLap/v1)) {
-                        overtakingCount++;
+                        numberOvertaking++;
                     }
                     p2 += (v2 * (double) lengthLap/v1) % lengthLap;
                 }
             }
         }
-
-        return overtakingCount;
+        return numberOvertaking;
     }
 
-    private static class Car implements ICar {
-
-        private int startPosition;
-
-        private int speed;
-
-        private Car(int startPosition, int speed) {
-            this.startPosition = startPosition;
-            this.speed = speed;
-        }
-
-        @Override
-        public int getStartPosition() {
-            return startPosition;
-        }
-
-        @Override
-        public int getSpeed() {
-            return speed;
-        }
-    }
 }
