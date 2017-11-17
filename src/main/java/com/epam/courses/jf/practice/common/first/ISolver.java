32834da9b9bc95a2a0a5a303975b055f9d7bd600
nbikbaev <niaz.bikbaev@gmail.com>
2017-11-17 10:49:00 +0300

    ...

--- src/main/java/com/epam/courses/jf/practice/common/first/ISolver.java
+++ src/main/java/com/epam/courses/jf/practice/common/first/ISolver.java
@@ -1,143 +1,143 @@
 package com.epam.courses.jf.practice.common.first;
 
 /**
- * First task interface
- * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Задание%20№1">Task description</a>}
+ * Интерфейс первого задания.
+ * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Задание%20№1">Описание задания</a>}
  */
 public interface ISolver {
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.1">Task #1</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.1">Задание №1</a>}
      */
     default void task1() { throw new UnsupportedOperationException("Task 1 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.2">Task #2</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.2">Задание №2</a>}
      */
     default void task2() { throw new UnsupportedOperationException("Task 2 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.3">Task #3</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.3">Задание №3</a>}
      */
     default void task3() { throw new UnsupportedOperationException("Task 3 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.4">Task #4</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.4">Задание №4</a>}
      */
     default void task4() { throw new UnsupportedOperationException("Task 4 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.5">Task #5</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.5">Задание №5</a>}
      */
     default void task5() { throw new UnsupportedOperationException("Task 5 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.6">Task #6</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.6">Задание №6</a>}
      */
     default void task6() { throw new UnsupportedOperationException("Task 6 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.7">Task #7</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.7">Задание №7</a>}
      */
     default void task7() { throw new UnsupportedOperationException("Task 7 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.8">Task #8</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.8">Задание №8</a>}
      */
     default void task8() { throw new UnsupportedOperationException("Task 8 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.9">Task #9</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.9">Задание №9</a>}
      */
     default void task9() { throw new UnsupportedOperationException("Task 9 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.10">Task #10</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.10">Задание №10</a>}
      */
     default void task10() { throw new UnsupportedOperationException("Task 10 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.11">Task #1</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.11">Задание №11</a>}
      */
     default void task11() { throw new UnsupportedOperationException("Task 11 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.12">Task #12</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.12">Задание №12</a>}
      */
     default void task12() { throw new UnsupportedOperationException("Task 12 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.13">Task #3</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.13">Задание №13</a>}
      */
     default void task13() { throw new UnsupportedOperationException("Task 13 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.14">Task #14</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.14">Задание №14</a>}
      */
     default void task14() { throw new UnsupportedOperationException("Task 14 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.15">Task #15</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.15">Задание №15</a>}
      */
     default void task15() { throw new UnsupportedOperationException("Task 15 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.16">Task #16</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.16">Задание №16</a>}
      */
     default void task16() { throw new UnsupportedOperationException("Task 16 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.17">Task #17</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.17">Задание №17</a>}
      */
     default void task17() { throw new UnsupportedOperationException("Task 17 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.18">Task #18</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.18">Задание №18</a>}
      */
     default void task18() { throw new UnsupportedOperationException("Task 18 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.19">Task #19</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.19">Задание №19</a>}
      */
     default void task19() { throw new UnsupportedOperationException("Task 19 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.20">Task #20</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.20">Задание №20</a>}
      */
     default void task20() { throw new UnsupportedOperationException("Task 20 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.21">Task #21</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.21">Задание №21</a>}
      */
     default void task21() { throw new UnsupportedOperationException("Task 21 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.22">Task #22</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.22">Задание №22</a>}
      */
     default void task22() { throw new UnsupportedOperationException("Task 22 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.23">Task #23</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.23">Задание №23</a>}
      */
     default void task23() { throw new UnsupportedOperationException("Task 23 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.24">Task #24</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.24">Задание №24</a>}
      */
     default void task24() { throw new UnsupportedOperationException("Task 24 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.25">Task #25</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.25">Задание №25</a>}
      */
     default void task25() { throw new UnsupportedOperationException("Task 25 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.26">Task #26</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.26">Задание №26</a>}
      */
     default void task26() { throw new UnsupportedOperationException("Task 26 not implemented yet."); }
 
     /**
-     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.27">Task #27</a>}
+     * {@see <a href="https://bitbucket.org/elefus/spring-java-school-epam-c-2017/wiki/Task%201.27">Задание №27</a>}
      */
     default void task27() { throw new UnsupportedOperationException("Task 27 not implemented yet."); }
 }
