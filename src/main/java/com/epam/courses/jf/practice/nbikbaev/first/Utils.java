8c84e2690da6ba405b63e797acbe7b310dcd67c4
nbikbaev <niaz.bikbaev@gmail.com>
2017-11-17 05:59:15 +0300

    transposeMatrix -> rotateMatrix

--- src/main/java/com/epam/courses/jf/practice/nbikbaev/first/Utils.java
+++ src/main/java/com/epam/courses/jf/practice/nbikbaev/first/Utils.java
@@ -103,16 +103,16 @@ public class Utils {
     }
 
     /**
-     * Returns transposed matrix
+     * Returns matrix rotated by 90 degrees
      *
-     * @param matrix Matrix to be transposed
-     * @return Transposed matrix
+     * @param matrix Matrix to be rotated
+     * @return Rotated matrix
      */
-    public static int[][] transposeMatrix(int[][] matrix) {
+    public static int[][] rotateMatrix(int[][] matrix) {
         int[][] transposedMatrix = new int[matrix[0].length][matrix.length];
         for (int i = 0; i < matrix.length; i++)
             for (int j = 0; j < matrix[0].length; j++)
-                transposedMatrix[j][i] = matrix[i][j];
+                transposedMatrix[j][i] = matrix[i][matrix[0].length - 1 - j];
         return transposedMatrix;
     }
 
