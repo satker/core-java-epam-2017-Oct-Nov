1c3f9d3978846e54478c4469fb6ae969eb437e4d
gulyaev13 <gulyaev13@gmail.com>
2017-11-18 00:08:47 +0300

    Task 2.11 updated: solution for ArrayList is address arithmetic, for LinkedList - Iterator

--- src/main/java/com/epam/courses/jf/practice/igulyaev/second/TestableTask11.java
+++ src/main/java/com/epam/courses/jf/practice/igulyaev/second/TestableTask11.java
@@ -5,26 +5,31 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Iterator;
-import java.util.ListIterator;
-import java.util.Arrays;
 
 public class TestableTask11 implements ITestableTask11 {
 
     public String emulate(ArrayList<String> peoples) {
-        return emulateList(peoples);
+        int n = peoples.size();
+        int current_index = -1;
+        for(int i = 0; i < n - 1; ++i) {
+            current_index += 1;
+            current_index %= peoples.size();
+            peoples.remove(current_index);
+        }
+        return peoples.get(0);
     }
-
 
     public String emulate(LinkedList<String> peoples) {
-        return emulateList(peoples);
-    }
-
-    private String emulateList(List<String> peoples) {
         int start = 0;
         while(peoples.size() > 1){
-            for(Iterator<String> i = peoples.listIterator(start); i.hasNext() && i.next()!= null && i.hasNext();){
-                i.remove();
+            Iterator<String> i = peoples.iterator();
+            if(start != 0){
+                i.next();
             }
+            do {
+                i.next();
+                i.remove();
+            } while(i.hasNext() && i.next() != null && i.hasNext());
             start = (peoples.size() + 1) % 2;
         }
         return peoples.get(0);
