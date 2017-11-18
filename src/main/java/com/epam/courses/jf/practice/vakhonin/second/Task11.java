e5fb5a2fb8aabf87a68e1c3607da8196e9af2457
vakhonin <isvakhonin@yandex.ru>
2017-11-18 16:17:00 +0300

    add_only_task2_11

--- src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task11.java
+++ src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task11.java
@@ -3,6 +3,7 @@ package com.epam.courses.jf.practice.vakhonin.second;
 import com.epam.courses.jf.practice.common.second.ITestableTask11;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
@@ -13,30 +14,53 @@ public class Task11 implements ITestableTask11{
     @Override
     public String emulate(ArrayList<String> peoples){
 
-        int size = peoples.size();
-        int j = 0;
-        int nextIndex = 0;
-        while(size!=1){
-            peoples.remove(nextIndex % size);
-            j++;
-            nextIndex = j % size;
-            size--;
-        }
 
+        Iterator<String> it;
+        int shift = 0;
+        while(peoples.size() != 1){
+            it = peoples.iterator();
+            if(shift == 1){
+                it.next();
+                shift--;
+            }
+            while(it.hasNext()){
+                if(shift == 0){
+                    System.out.println(it.next());
+
+                    it.remove();
+                    shift++;
+                }
+                else{
+                    it.next();
+                    shift--;
+                }
+            }
+        }
         return peoples.get(0);
     }
 
     @Override
     public String emulate(LinkedList<String> peoples){
+        Iterator<String> it;
+        int shift = 0;
+        while(peoples.size() != 1){
+            it = peoples.iterator();
+            if(shift == 1){
+                it.next();
+                shift--;
+            }
+            while(it.hasNext()){
+                if(shift == 0){
+                    System.out.println(it.next());
 
-        int size = peoples.size();
-        int j = 0;
-        int nextIndex = 0;
-        while(size!=1){
-            peoples.remove(nextIndex % size);
-            j++;
-            nextIndex = j % size;
-            size--;
+                    it.remove();
+                    shift++;
+                }
+                else{
+                    it.next();
+                    shift--;
+                }
+            }
         }
         return peoples.get(0);
     }
