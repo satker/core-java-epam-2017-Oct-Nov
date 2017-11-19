2dd4705b993c7c9f3aee03079673e13f36177838
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-19 05:51:37 +0300

    Заменил куски ввода и вывода матриц в tasks на вызов методов класса Matrix

--- src/main/java/com/epam/courses/jf/practice/sskovalevskiy/first/Matrix.java
+++ src/main/java/com/epam/courses/jf/practice/sskovalevskiy/first/Matrix.java
@@ -7,6 +7,10 @@ import java.util.Scanner;
  */
 public class Matrix {
 
+    /**
+     * @param scanner - принимает на вход Scanner
+     * @return Integer[][] matrix - возвращает прочитанную с консоли матрицу
+     */
     public static Integer[][] readMatrix(Scanner scanner) {
         final int DIMENSION = scanner.nextInt();
         Integer[][] matrix = new Integer[DIMENSION][DIMENSION];
@@ -18,7 +22,28 @@ public class Matrix {
         return matrix;
     }
 
+    /**
+     * @param matrix метод принимает на вход матрицу Integer[][] matrix
+     * выводит в консоль размерность матрицы и её саму
+     */
     public static void printMatrix(Integer[][] matrix) {
+
+        System.out.println(matrix.length);
+
+        for (int row = 0; row < matrix.length; ++row) {
+            for (int col = 0; col < matrix.length; ++col) {
+                System.out.print(matrix[row][col] + "\t");
+            }
+        }
+        System.out.println();
+    }
+
+    /**
+     * @param matrix метод принимает на вход матрицу int[][] matrix
+     * выводит в консоль размерность матрицы и её саму
+     */
+    public static void printMatrix(int[][] matrix) {
+
         System.out.println(matrix.length);
 
         for (int row = 0; row < matrix.length; ++row) {
