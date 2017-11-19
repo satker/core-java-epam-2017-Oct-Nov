cf618130b9349fb87a9ca7854ea6aece0c9c7f25
vakhonin <isvakhonin@yandex.ru>
2017-11-19 13:04:20 +0300

    add_only_task2_12

--- src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task12.java
+++ src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task12.java
@@ -1,7 +1,25 @@
 package com.epam.courses.jf.practice.vakhonin.second;
 
+import com.epam.courses.jf.practice.common.second.ITestableTask12;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * Created by igorvahonin on 13.11.17.
  */
-public class Task12 {
+public class Task12 implements ITestableTask12{
+
+    @Override
+    public List<Integer> transform(List<Integer> integers, int value) {
+        Set<Integer> set = new TreeSet<>(integers);
+
+        List<Integer> list = new ArrayList<>(set);
+
+
+
+        return list;
+    }
 }
