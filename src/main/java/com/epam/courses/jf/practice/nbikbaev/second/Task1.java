67e45136d345daa3d6275f362b43dd52cd707efc
nbikbaev <niaz.bikbaev@gmail.com>
2017-11-20 07:06:07 +0300

    Task19 test #3

--- src/main/java/com/epam/courses/jf/practice/nbikbaev/second/Task1.java
+++ src/main/java/com/epam/courses/jf/practice/nbikbaev/second/Task1.java
@@ -9,18 +9,18 @@ public class Task1 implements ITestableTask1 {
     @Override
     public List<String> reverseFile(File input, File output) {
         List<String> strings = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(input))) {
-            String line = bufferedReader.readLine();
-            strings.add(line);
+        try (Scanner scanner = new Scanner(input)) {
+            while (scanner.hasNext()) {
+                strings.add(scanner.next());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
         Collections.reverse(strings);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(output))) {
-            for (int i = 0; i < strings.size(); i++) {
-                bufferedWriter.write(strings.get(i));
+            for (String string : strings) {
+                bufferedWriter.write(string);
                 bufferedWriter.newLine();
-                bufferedWriter.flush();
             }
         } catch (IOException e) {
             e.printStackTrace();
