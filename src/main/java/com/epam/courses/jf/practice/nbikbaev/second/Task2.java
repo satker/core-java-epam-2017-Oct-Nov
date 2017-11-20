8803f094bf0bdc8b8c250c9ebbf48d8f45b36f34
nbikbaev <niaz.bikbaev@gmail.com>
2017-11-20 07:18:35 +0300

    Task2 test #1

---
+++ src/main/java/com/epam/courses/jf/practice/nbikbaev/second/Task2.java
@@ -0,0 +1,21 @@
+package com.epam.courses.jf.practice.nbikbaev.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask2;
+
+import java.io.File;
+import java.util.*;
+
+public class Task2 implements ITestableTask2 {
+    @Override
+    public Set<File> getFiles(File directory) {
+        Set<File> files = new HashSet<>();
+        Collection<File> fileCollection = Arrays.asList(directory.listFiles());
+        for (File file : fileCollection) {
+            if (file.isDirectory()) {
+                files.addAll(Arrays.asList(file.listFiles()));
+            }
+        }
+        files.addAll(fileCollection);
+        return files;
+    }
+}
