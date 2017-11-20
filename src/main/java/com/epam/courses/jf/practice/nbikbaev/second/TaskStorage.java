fbe3deb34f98450dd6c4fd52280fdcc4b80fb52d
nbikbaev <niaz.bikbaev@gmail.com>
2017-11-20 07:18:49 +0300

    test

--- src/main/java/com/epam/courses/jf/practice/nbikbaev/second/TaskStorage.java
+++ src/main/java/com/epam/courses/jf/practice/nbikbaev/second/TaskStorage.java
@@ -1,11 +1,19 @@
 package com.epam.courses.jf.practice.nbikbaev.second;
 
 import com.epam.courses.jf.practice.common.second.ITaskStorage;
+import com.epam.courses.jf.practice.common.second.ITestableTask1;
+import com.epam.courses.jf.practice.common.second.ITestableTask2;
 
 public class TaskStorage implements ITaskStorage {
 
     @Override
     public <T extends ITestableTask> T getSolver(Class<T> taskInterface) {
-        return (T) new Task1();
+        if (ITestableTask1.class.equals(taskInterface)) {
+            return (T) new Task1();
+        }
+        if (ITestableTask2.class.equals(taskInterface)) {
+            return (T) new Task2();
+        }
+        return null;
     }
 }
