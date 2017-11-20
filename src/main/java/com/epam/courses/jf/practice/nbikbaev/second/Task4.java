bb2edad2468e313d61bb9b5dc2a580e13c70386d
nbikbaev <niaz.bikbaev@gmail.com>
2017-11-20 07:51:20 +0300

    Task4 done

--- src/main/java/com/epam/courses/jf/practice/nbikbaev/second/Task4.java
+++ src/main/java/com/epam/courses/jf/practice/nbikbaev/second/Task4.java
@@ -2,19 +2,22 @@ package com.epam.courses.jf.practice.nbikbaev.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask4;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Task4 implements ITestableTask4 {
 
     @Override
     public Set<Integer> intersection(Set<Integer> first, Set<Integer> second) {
-        first.retainAll(second);
-        return first;
+        Set<Integer> intersectionSet = new HashSet<>(first);
+        intersectionSet.retainAll(second);
+        return intersectionSet;
     }
 
     @Override
     public Set<Integer> union(Set<Integer> first, Set<Integer> second) {
-        first.addAll(second);
-        return first;
+        Set<Integer> unionSet = new HashSet<>(first);
+        unionSet.addAll(second);
+        return unionSet;
     }
 }
