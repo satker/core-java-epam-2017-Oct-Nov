17eb9affca346e1fbf8edc93c7b61c9b14c084b6
ndevyatiy <hvoltage9@yandex.ru>
2017-11-20 19:27:37 +0300

    отредактирован TestableTask4 Добавлены условия EMPTY_SET Создана копия множества set, чтобы не менять исходные множества

--- src/main/java/com/epam/courses/jf/practice/NikDevyatyi/second/TestableTask4.java
+++ src/main/java/com/epam/courses/jf/practice/NikDevyatyi/second/TestableTask4.java
@@ -2,22 +2,32 @@ package com.epam.courses.jf.practice.NikDevyatyi.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask4;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 public class TestableTask4 implements ITestableTask4 {
     @Override
     public Set<Integer> intersection(Set<Integer> first, Set<Integer> second) {
-        Set<Integer> result;
-        result = first;
-        result.retainAll(second);
-        return result;
+        Set<Integer> result = new HashSet<>();
+        result.addAll(first);
+        if(!result.isEmpty()){
+            result.retainAll(second);
+        }
+        return (Set<Integer>) result;
     }
 
     @Override
     public Set<Integer> union(Set<Integer> first, Set<Integer> second) {
-        Set<Integer> result;
-        result = first;
-        result.addAll(second);
-        return result;
+        Set<Integer> result = new HashSet<>();
+        result.addAll(first);
+        if(first.isEmpty()&&second.isEmpty()){
+            return Collections.EMPTY_SET;
+        }
+        else if(!first.isEmpty()){
+            result.addAll(second);
+            return (Set<Integer>) result;
+        }
+        return second;
     }
 }
