178f992efedde2ed18cfcd2d9e6d213ff3e7dee3
nbikbaev <niaz.bikbaev@gmail.com>
2017-11-20 07:48:16 +0300

    test

--- src/main/java/com/epam/courses/jf/practice/nbikbaev/second/TaskStorage.java
+++ src/main/java/com/epam/courses/jf/practice/nbikbaev/second/TaskStorage.java
@@ -6,16 +6,16 @@ public class TaskStorage implements ITaskStorage {
 
     @Override
     public <T extends ITestableTask> T getSolver(Class<T> taskInterface) {
-        if (ITestableTask1.class.equals(taskInterface)) {
+        if (taskInterface == ITestableTask1.class){
             return (T) new Task1();
         }
-        if (ITestableTask2.class.equals(taskInterface)) {
+        if (taskInterface == ITestableTask2.class){
             return (T) new Task2();
         }
-        if (ITestableTask3.class.equals(taskInterface)) {
+        if (taskInterface == ITestableTask3.class){
             return (T) new Task3();
         }
-        if (ITestableTask4.class.equals(taskInterface)) {
+        if (taskInterface == ITestableTask4.class){
             return (T) new Task4();
         }
         return null;
