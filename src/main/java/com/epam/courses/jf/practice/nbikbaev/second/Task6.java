b8443a8cd08291ecf41538fe678e008977c192b6
nbikbaev <niaz.bikbaev@gmail.com>
2017-11-21 21:55:28 +0300

    Task 6 done

---
+++ src/main/java/com/epam/courses/jf/practice/nbikbaev/second/Task6.java
@@ -0,0 +1,31 @@
+package com.epam.courses.jf.practice.nbikbaev.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask6;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task6 implements ITestableTask6 {
+    @Override
+    public HashMap<Integer, Integer> addPolynomials(HashMap<Integer, Integer> first, HashMap<Integer, Integer> second) {
+        Set<Integer> integerSet = new HashSet<>();
+        integerSet.addAll(first.keySet());
+        integerSet.addAll(second.keySet());
+        HashMap<Integer, Integer> result = new HashMap<>();
+        int sum;
+        for (int i : integerSet) {
+            sum = 0;
+            if (first.containsKey(i)) {
+                sum += first.get(i);
+            }
+            if (second.containsKey(i)) {
+                sum += second.get(i);
+            }
+            result.put(i, sum);
+        }
+        return result;
+    }
+
+}
