8dd7e7bcb680d25eeac755d675cfa538af8e3583
SigarevVL <SigarevVL94@gmail.com>
2017-11-21 23:15:39 +0300

    add task 12 from the second part new

--- src/main/java/com/epam/courses/jf/practice/SigarevVL/second/TaskStorage.java
+++ src/main/java/com/epam/courses/jf/practice/SigarevVL/second/TaskStorage.java
@@ -43,9 +43,9 @@ public class TaskStorage implements ITaskStorage {
         if (taskInterface == ITestableTask11.class){
             return (T) new Task11();
         }
-//        if (taskInterface == ITestableTask12.class){
-//            return (T) new Task12();
-//        }
+        if (taskInterface == ITestableTask12.class){
+            return (T) new Task12();
+        }
 //        if (taskInterface == ITestableTask13.class){
 //            return (T) new Task13();
 //        }
