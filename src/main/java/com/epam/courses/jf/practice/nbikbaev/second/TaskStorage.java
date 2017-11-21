51002490c84f896a4ddeff2f3674ac302d104d07
nbikbaev <niaz.bikbaev@gmail.com>
2017-11-21 22:58:31 +0300

    Added Task7 implementation

--- src/main/java/com/epam/courses/jf/practice/nbikbaev/second/TaskStorage.java
+++ src/main/java/com/epam/courses/jf/practice/nbikbaev/second/TaskStorage.java
@@ -21,6 +21,12 @@ public class TaskStorage implements ITaskStorage {
         if (taskInterface == ITestableTask5.class){
             return (T) new Task5();
         }
+        if (taskInterface == ITestableTask6.class){
+            return (T) new Task6();
+        }
+        if (taskInterface == ITestableTask7.class){
+            return (T) new Task7();
+        }
         return null;
     }
 }
