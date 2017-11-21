85640ae76993d7308d30823a0d617318922cf27f
nbikbaev <niaz.bikbaev@gmail.com>
2017-11-22 00:02:47 +0300

    Task 10 testing

---
+++ src/main/java/com/epam/courses/jf/practice/nbikbaev/second/Task10.java
@@ -0,0 +1,28 @@
+package com.epam.courses.jf.practice.nbikbaev.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask10;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Task10 implements ITestableTask10 {
+    @Override
+    public HashMap<String, Integer> countNumberWords(File input) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        try (Scanner scanner = new Scanner(input)) {
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                if (hashMap.containsKey(word)) {
+                    int count = hashMap.get(word);
+                    count++;
+                    hashMap.put(word, count);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return hashMap;
+    }
+}
